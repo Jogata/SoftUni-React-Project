448048b9ae44e9e219d5bf35c51f88913d39a322
Yonko Nikolov <jonkoslav@abv.bg>
2024-08-27 14:19:07 +0300

    memo hook tutorial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ import Logout from './components/logout/Logout'
 // ============================================================
 // import { useHistory, Switch } from 'react-router-dom'
 import { Link, useParams } from 'react-router-dom'
-import { useEffect, useState, useReducer } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
 function Header({ title, width }) {
   return (
@@ -468,83 +468,43 @@ function App() {
     }
   }
 
-  function UserForm() {
-    const [state, dispatch] = useReducer(
-      (state, action) => ({
-        ...state,
-        ...action,
-      }),
-      {
-        first: "",
-        last: "",
-      }
+  function TestApp() {
+    const [numbers] = useState([10, 20, 30]);
+
+    const total = useMemo(
+      () => numbers.reduce((acc, number) => acc + number, 0),
+      [numbers]
     );
+
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const countTotal = count1 + count2;
+
+    const [names] = useState(["John", "Paul", "George", "Ringo"]);
+
+    const sortedNames = () => {
+      console.log("sorted names without memo");
+      return [...names].sort();
+    };
+
+    const sortedNamesMemo = useMemo(() => {
+      console.log("sorted names with memo");
+      return [...names].sort();
+    }, [names]);
 
     return (
-      <div>
-        <input
-          type="text"
-          value={state.first}
-          onChange={(e) => dispatch({ first: e.target.value })}
-        />
-        <input
-          type="text"
-          value={state.last}
-          onChange={(e) => dispatch({ last: e.target.value })}
-        />
-        <div>First: {state.first}</div>
-        <div>Last: {state.last}</div>
-      </div>
+      <>
+        <div>Total: {total}</div>
+        <div>Names: {names.join(", ")}</div>
+        <div>Sorted Names: {sortedNames().join(", ")}</div>
+        <div>Sorted Names with memo: {sortedNamesMemo.join(", ")}</div>
+        <button onClick={() => setCount1(count1 + 1)}>Count1: {count1}</button>
+        <button onClick={() => setCount2(count2 + 1)}>Count2: {count2}</button>
+        <div>Total: {countTotal}</div>
+      </>
     );
   }
-
-function NameList() {
-  const [state, dispatch] = useReducer(
-    (state, action) => {
-      switch (action.type) {
-        case "SET_NAME":
-          return { ...state, name: action.payload };
-        case "ADD_NAME":
-          return {
-            ...state,
-            names: [...state.names, state.name],
-            name: "",
-          };
-      }
-    },
-    {
-      names: [],
-      name: "",
-    }
-  );
-
-  return (
-    <div className="list" style={{flex: 1}}>
-      <div>
-        {state.names.map((name, index) => (
-          <div key={index}>{name}</div>
-        ))}
-      </div>
-      <input
-        type="text"
-        value={state.name}
-        onChange={(e) =>
-          dispatch({ type: "SET_NAME", payload: e.target.value })
-        }
-      />
-      <button 
-        onClick={() => dispatch({ type: "ADD_NAME" })}
-        style={{
-          color: "black", 
-          backgroundColor: "whitesmoke", 
-          padding: "0.5em 1.2em"
-        }}
-      >
-        Add Name
-      </button>
-    </div>
-  );
-}
 
   return (
     <>
@@ -553,8 +513,7 @@ function NameList() {
 
           <Header title="React JS Blog" width={width} />
           <Nav search={search} setSearch={setSearch} />
-          <UserForm />
-          <NameList />
+          <TestApp />
           {/* <Routes>
             <Route path='/' element={<Home posts={searchResults} />} />
             <Route path='/post' element={<NewPost handleSubmit={handleSubmit} postTitle={postTitle} setPostTitle={setPostTitle} postBody={postBody} setPostBody={setPostBody} />} />
