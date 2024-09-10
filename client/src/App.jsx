bea36fcb235fa4780f7dc1583d58b1bdf04d6bb5
Yonko Nikolov <jonkoslav@abv.bg>
2024-09-10 13:11:36 +0300

    ref hook in class component exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -499,18 +499,31 @@ class RefDemo extends Component {
   constructor(props) {
     super(props);
     this.inputRef = createRef();
+    this.cbRef = null;
+    this.setCbRef = (element) => {
+      this.cbRef = element;
+    }
   }
 
   componentDidMount() {
-    console.log(this.inputRef);
-    console.log(this.inputRef.current);
-    this.inputRef.current.focus();
+    // console.log(this.inputRef);
+    // console.log(this.inputRef.current);
+    // this.inputRef.current.focus();
+    if (this.cbRef) {
+      this.cbRef.focus();
+    }
+  }
+
+  clickHandler() {
+    console.log(this.inputRef.current.value);
   }
 
   render() {
     return (
       <div style={{flex: 1, paddingTop: "2rem"}}>
         <input type="text" ref={this.inputRef} />
+        <input type="text" ref={this.setCbRef} />
+        <button onClick={() => this.clickHandler()}>Click</button>
       </div>
     )
   }
