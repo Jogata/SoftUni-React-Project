09c592b3aeb5e2eee4b68b9b18b38233f44b0bb1
Yonko Nikolov <jonkoslav@abv.bg>
2024-09-24 13:34:27 +0300

    use state exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -524,6 +524,63 @@ const UseStateObject = () => {
   );
 };
 
+
+const UseStateCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      console.log("timeout");
+      // setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button 
+          className='btn' 
+          onClick={() => setValue(value - 1)}
+        >
+          decrease
+        </button>
+        <button 
+          className='btn' 
+          onClick={reset}
+        >
+          reset
+        </button>
+        <button 
+          className='btn' 
+          onClick={() => setValue(value + 1)}
+        >
+          increase
+        </button>
+      </section>
+      <hr />
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button 
+          className='btn' 
+          onClick={complexIncrease}
+        >
+          increase later
+        </button>
+      </section>
+    </>
+  );
+};
+
 function App() {
   return (
     <>
@@ -531,7 +588,8 @@ function App() {
         <div className="body">
           <Header title="React JS Blog" />
           <div className="container">
-            <UseStateObject />
+            {/* <UseStateObject /> */}
+            <UseStateCounter />
           </div>
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
