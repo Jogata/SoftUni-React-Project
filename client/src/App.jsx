fecf4d61a455e4996ce6fea555212ba7b8ffaed6
Yonko Nikolov <jonkoslav@abv.bg>
2024-09-25 19:13:02 +0300

    use effect exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -553,6 +553,43 @@ const UseEffectCleanup = () => {
   );
 };
 
+
+const UseEffectFetchData = () => {
+  const [users, setUsers] = useState([]);
+  
+  const url = 'https://api.github.com/users';
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+    // console.log(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
 function App() {
   return (
     <>
@@ -562,6 +599,7 @@ function App() {
           <div className="container">
             <UseEffectBasics />
             <UseEffectCleanup />
+            <UseEffectFetchData />
           </div>
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
