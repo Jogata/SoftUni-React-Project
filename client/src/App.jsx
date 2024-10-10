7f3e2941d07d17a2710701eed34eda4bdbabf47a
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-10 17:39:35 +0300

    menu - exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -495,74 +495,153 @@ function useFetch(url) {
 const baseURL = "http://localhost:3030/jsonstore/blog/"
 // ============================================================
 
-const data = [
-  { 
-    id: 1,  
-    title: 'Do I have to allow the use of cookies?',
-    info:
-      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
-  },  
-  { 
-    id: 2,  
-    title: 'How do I change my My Page password?',
-    info:
-      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
-  },  
-  { 
-    id: 3,  
-    title: 'What is BankID?',
-    info:
-      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
-  },  
-  { 
-    id: 4,  
-    title: 'Whose birth number can I use?',
-    info:
-      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
-  },  
+const menu = [
   {
-    id: 5,
-    title: 'When do I recieve a password ordered by letter?',
-    info:
-      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
-  },
-]
+    id: 1,
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    img: './images/item-1.jpeg',
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },    
+  {     
+    id: 2,
+    title: 'diner double',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },    
+  {   
+    id: 3,
+    title: 'godzilla milkshake',
+    category: 'shakes',
+    price: 6.99,
+    img: './images/item-3.jpeg',
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },    
+  {     
+    id: 4,
+    title: 'country delight',
+    category: 'breakfast',
+    price: 20.99,
+    img: './images/item-4.jpeg',
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },    
+  {   
+    id: 5,  
+    title: 'egg attack',
+    category: 'lunch',
+    price: 22.99,
+    img: './images/item-5.jpeg',
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },  
+  { 
+    id: 6,
+    title: 'oreo dream',
+    category: 'shakes',
+    price: 18.99,
+    img: './images/item-6.jpeg',
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },  
+  { 
+    id: 7,
+    title: 'bacon overflow',
+    category: 'breakfast',
+    price: 8.99,
+    img: './images/item-7.jpeg',
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },  
+  { 
+    id: 8,
+    title: 'american classic',
+    category: 'lunch',
+    price: 12.99,
+    img: './images/item-8.jpeg',
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },  
+  { 
+    id: 9,
+    title: 'quarantine buddy',
+    category: 'shakes',
+    price: 16.99,
+    img: './images/item-9.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },   
+];  
 
-const SingleQuestion = ({ title, info }) => {
-  const [showInfo, setShowInfo] = useState(false);
-  return (
-    <article className='question'>
-      <header>
-        <h4>{title}</h4>
-        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
-          {/* {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} */}
-          {showInfo ? "-" : "+"}
-        </button>
-      </header>
-      {showInfo && <p>{info}</p>}
-    </article>
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
+
+const Menu = ({ items }) => {
+  return (  
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+            <div className='item-info'>
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>${price}</h4>
+              </header>
+              <p className='item-text'>{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
   );
-}
+};
+
+const Categories = ({ categories, filterItems }) => {
+  return (  
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button" 
+            className="filter-btn" 
+            key={index} 
+            onClick={() => filterItems(category)} 
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );  
+};
 
 function App() {
-  const [questions, setQuestions] = useState(data);
+
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <>
       <AuthContextProvider>
         <div className="body">
           <Header title="React JS Blog" />
-          <main>
-            <div className="grid-container">
-              <h3>questions and answers about login</h3>
-              <section className='info'>
-                {questions.map((question) => {
-                  return (
-                    <SingleQuestion key={question.id} {...question}></SingleQuestion>
-                  );
-                })}
-              </section>
-            </div>
-          </main>
+          <main>  
+            <section className="menu section">
+              <div className="title">
+                <h2>our menu</h2>
+                <div className="underline"></div>
+              </div>
+              <Categories categories={categories} filterItems={filterItems} />
+              <Menu items={menuItems} />
+            </section>
+          </main> 
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
           {/* <DataProvider>
