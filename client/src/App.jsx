a28f079a1518d090fdcbcdc1621a6e833154ed07
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-10 10:44:22 +0300

    accordions - exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -495,120 +495,71 @@ function useFetch(url) {
 const baseURL = "http://localhost:3030/jsonstore/blog/"
 // ============================================================
 
-const people = [
+const data = [
+  { 
+    id: 1,  
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+  },  
+  { 
+    id: 2,  
+    title: 'How do I change my My Page password?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },  
+  { 
+    id: 3,  
+    title: 'What is BankID?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },  
+  { 
+    id: 4,  
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },  
   {
-    id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
   },
-  {
-    id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-    text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
-  },
-  {
-    id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-  },
-  {
-    id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-    text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
-  },
-];  
+]
 
-// import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-
-const Review = () => {
-  const [index, setIndex] = useState(0);
-  const { name, job, image, text } = people[index];
-  const checkNumber = (number) => {
-    if (number > people.length - 1) {
-      return 0;
-    }
-    if (number < 0) {
-      return people.length - 1;
-    }
-    return number;
-  };
-  const nextPerson = () => {
-    setIndex((index) => {
-      let newIndex = index + 1;
-      return checkNumber(newIndex);
-    });
-  };
-  const prevPerson = () => {
-    setIndex((index) => {
-      let newIndex = index - 1;
-      return checkNumber(newIndex);
-    });
-  };
-  const randomPerson = () => {
-    let randomNumber = Math.floor(Math.random() * people.length);
-    if (randomNumber === index) {
-      randomNumber = index + 1;
-    }
-    setIndex(checkNumber(randomNumber));
-  };
-
-  return (  
-    <article className='review'>
-      <div className='img-container'>
-        <img src={image} alt={name} className='person-img' />
-        <span className='quote-icon'>
-          {/* <FaQuoteRight /> */}
-          &quot;
-        </span>
-      </div>
-      <h4 className='author'>{name}</h4>
-      <p className='job'>{job}</p>
-      <p className='info'>{text}</p>
-      <div className='button-container'>
-        <button className='prev-btn' onClick={prevPerson}>
-          {/* <FaChevronLeft /> */}
-          &laquo;
+const SingleQuestion = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {/* {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} */}
+          {showInfo ? "-" : "+"}
         </button>
-        <button className='next-btn' onClick={nextPerson}>
-          {/* <FaChevronRight /> */}
-          &raquo;
-        </button> 
-      </div>
-      <button className='random-btn' onClick={randomPerson}>
-        surprise me
-      </button>
-    </article>  
-  ); 
-}; 
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
+}
 
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
     <>
       <AuthContextProvider>
         <div className="body">
           <Header title="React JS Blog" />
-          <div className="container">
-            <div className='title'>
-              <h2>our reviews</h2>
-              <div className='underline'></div>
-            </div>
-            <Review />
+          <div className="grid-container">
+            <h3>questions and answers about login</h3>
+            <section className='info'>
+              {questions.map((question) => {
+                return (
+                  <SingleQuestion key={question.id} {...question}></SingleQuestion>
+                );
+              })}
+            </section>
           </div>
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
