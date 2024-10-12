be0320901306b8cc091783f0a0dd4232098e7d16
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-12 10:00:37 +0300

    slider - exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -495,121 +495,108 @@ function useFetch(url) {
 const baseURL = "http://localhost:3030/jsonstore/blog/"
 // ============================================================
 
-const url = 'https://course-api.com/react-tabs-project';
-
 const data = [
-  {
-    "id": "recAGJfiU4CeaV0HL",
-    "order": 3,
-    "title": "Full Stack Web Developer",
-    "dates": "December 2015 - Present",
-    "duties": [
-      "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
-      "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
-      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian."
-    ],
-    "company": "TOMMY"
-  },
-  {
-    "id": "recIL6mJNfWObonls",
-    "order": 2,
-    "title": "Front-End Engineer",
-    "dates": "May 2015 - December 2015",
-    "duties": [
-      "Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.",
-      "Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester",
-      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
-      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1"
-    ],
-    "company": "BIGDROP"
-  },
-  {
-    "id": "rec61x18GVY99hQq5",
-    "order": 1,
-    "title": "Engineering Intern",
-    "dates": "May 2014 - September 2015",
-    "duties": [
-      "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
-      "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
-      "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism."
-    ],
-    "company": "CUKER"
-  }
-]
+  { 
+    id: 1,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },  
+  { 
+    id: 2,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },  
+  { 
+    id: 3,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },  
+  { 
+    id: 4,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },  
+];  
 
-function Tabs() {
-  const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState(0);
+function Slider() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
 
-  const fetchJobs = async () => {
-    // const reponse = await fetch(url);
-    // const newJobs = await reponse.json();
-    setTimeout(() => {      
-      // setJobs(newJobs);
-      setJobs(data);
-      setLoading(false);
-    }, 2000);
-  }
+  useEffect(() => { 
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
+  useEffect(() => { 
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
 
-  if (loading) {
-    return (
-      <section className="section loading">
-        {/* <h1>Loading...</h1> */}
-        <Loader />
-      </section>
-    )
-  }
+  return (  
+    <section className="section"> 
+      <div className="title"> 
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>  
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
 
-  const { company, dates, duties, title } = jobs[value];
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
 
-  return (
-    <section className="section">
-      <div className="title">
-        <h2>experience</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        {/* btn container */}
-        <div className="btn-container">
-          {jobs.map((item, index) => {
-            return (
-              <button 
-                key={item.id} 
-                onClick={() => setValue(index)} 
-                className={`job-btn ${index === value && 'active-btn'}`} 
-              >
-                {item.company}
-              </button>
-            )
-          })}
-        </div>
-        {/* job info */}
-        <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div key={index} className="job-desc">
-                {/* <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight> */}
-                <span className='job-icon'>{">>"}</span>
-                <p>{duty}</p>
-              </div>
-            )
-          })}
-        </article>
-      </div>
-      <button type="button" className="btn">
-        more info
-      </button>
-    </section>
-  )
-}
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <span className="icon">{'"'}</span>
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          {/* <FiChevronLeft /> */}
+          {"<"}
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          {/* <FiChevronRight /> */}
+          {">"}
+        </button>
+      </div>  
+    </section>  
+  ) 
+} 
 
 function App() {
   return (
@@ -617,7 +604,7 @@ function App() {
       <AuthContextProvider>
         <div className="body">
           <Header title="React JS Blog" />
-          <Tabs />
+          <Slider />
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
           {/* <DataProvider>
