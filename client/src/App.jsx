7c8f61e13cc860610008bc2cab6de18223249396
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-15 10:59:57 +0300

    navigation - exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -495,147 +495,118 @@ function useFetch(url) {
 const baseURL = "http://localhost:3030/jsonstore/blog/"
 // ============================================================
 
-// const getLocalStorage = () => {
-//   let list = localStorage.getItem('list');
-//   if (list) {
-//     return (list = JSON.parse(localStorage.getItem('list')));
-//   } else {
-//     return [];
-//   }
-// };
+const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'home',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+];
 
-const GroceryList = ({ items, removeItem, editItem }) => {
-  return (  
-    <div className='grocery-list'>
-      {items.map((item) => {
-        const { id, title } = item;
-        return (
-          <article className='grocery-item' key={id}>
-            <p className='title'>{title}</p>
-            <div className='btn-container'>
-              <button
-                type='button'
-                className='edit-btn'
-                onClick={() => editItem(id)}
-              >
-                Edit
-              </button>
-              <button
-                type='button'
-                className='delete-btn'
-                onClick={() => removeItem(id)}
-              >
-                Delete
-              </button>
-            </div>
-          </article>
-        );
-      })}
-    </div>
-  );
-};
+const socials = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    icon: "facebook",
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: "twitter",
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    icon: "linkedin",
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: "behance",
+  },
+];
 
-const Alert = ({ type, msg, removeAlert, list }) => {
-  useEffect(() => { 
-    const timeout = setTimeout(() => {  
-      removeAlert();
-    }, 3000); 
-    return () => clearTimeout(timeout); 
-  }, [list]);
-  return <p className={`alert alert-${type}`}>{msg}</p>;
-};
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <a href="#" className="logo"></a>
+          <button className='nav-toggle' onClick={toggleLinks}>
+            menu
+          </button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {socials.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );  
+};  
 
 function App() {
-  const [name, setName] = useState('');
-  // const [list, setList] = useState(getLocalStorage());
-  const [list, setList] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name) {
-      showAlert(true, 'danger', 'please enter value');
-    } else if (name && isEditing) {
-      setList(
-        list.map((item) => {
-          if (item.id === editID) {
-            return { ...item, title: name };
-          }
-          return item;
-        })
-      );
-      setName('');
-      setEditID(null);
-      setIsEditing(false);
-      showAlert(true, 'success', 'value changed');
-    } else {
-      showAlert(true, 'success', 'item added to the list');
-      const newItem = { id: new Date().getTime().toString(), title: name };
-
-      setList([...list, newItem]);
-      setName('');
-    }
-  };
-
-  const showAlert = (show = false, type = '', msg = '') => {
-    setAlert({ show, type, msg });
-  };
-
-  const clearList = () => {
-    showAlert(true, 'danger', 'empty list');
-    setList([]);
-  };
-
-  const removeItem = (id) => {
-    showAlert(true, 'danger', 'item removed');
-    setList(list.filter((item) => item.id !== id));
-  };
-
-  const editItem = (id) => {
-    const specificItem = list.find((item) => item.id === id);
-    setIsEditing(true);
-    setEditID(id);
-    setName(specificItem.title);
-  };
-
-  // useEffect(() => {
-    // localStorage.setItem('list', JSON.stringify(list));
-  // }, [list]);
-
   return (
     <>
       <AuthContextProvider>
         <div className="body">
           <Header title="React JS Blog" />
-          <section className='section-center'>
-            <form className='grocery-form' onSubmit={handleSubmit}>
-              {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-
-              <h3>grocery bud</h3>
-              <div className='form-control'>
-                <input
-                  type='text'
-                  className='grocery'
-                  placeholder='e.g. eggs'
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-                <button type='submit' className='submit-btn'>
-                  {isEditing ? 'edit' : 'submit'}
-                </button>
-              </div>
-            </form>
-            {list.length > 0 && (
-              <div className='grocery-container'>
-                <GroceryList items={list} removeItem={removeItem} editItem={editItem} />
-                <button className='clear-btn' onClick={clearList}>
-                  clear items
-                </button>
-              </div>
-            )}
-          </section>
+          <Navbar />
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
           {/* <DataProvider>
