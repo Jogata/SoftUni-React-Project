e89a91421bce4ffa9b2e75891f879fd63d06dabd
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-17 14:54:08 +0300

    exercise - stripe submenus - added context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -549,10 +549,58 @@ const sublinks = [
   },  
 ];  
 
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
 function App() {
   return (
     <>
-      <AuthContextProvider>
+      {/* <AuthContextProvider> */}
+      <AppProvider>
         <div className="body">
           <Header title="React JS Blog" />
           <h1>Navbar </h1>
@@ -608,7 +656,8 @@ function App() {
           </svg>
 
         </div>
-      </AuthContextProvider>
+      {/* </AuthContextProvider> */}
+      </AppProvider>
     </>
   )
 }
