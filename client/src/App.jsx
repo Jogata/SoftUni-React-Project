c45dc19dc7c87b61e8f6b453a2aac60764e34546
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-18 12:20:25 +0300

    exercise - stripe submenus - added submenu component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -670,6 +670,56 @@ const Navbar = () => {
   );
 };
 
+const Submenu = () => {
+  const {
+    isSubmenuOpen,
+    page: { page, links },
+    location,
+  } = useGlobalContext();
+
+  const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
+
+  useEffect(() => {
+    setColumns('col-2');
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+    console.log(links);
+
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+
+    if (links.length > 3) {
+      setColumns('col-4');
+    }
+  }, [page, location, links]);
+
+  return (
+    <aside
+      className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+      ref={container}
+    >
+      <section>
+        <h4>{page}</h4>
+        <div className={`submenu-center ${columns}`}>
+          {links.map((link, index) => {
+            const { url, icon, label } = link
+            return (
+              <a key={index} href={url}>
+                {icon}
+                {label}
+              </a>
+            )
+          })}
+        </div>
+      </section>
+    </aside>
+  )
+}
+
 function App() {
   return (
     <>
@@ -678,7 +728,7 @@ function App() {
         <div className="body">
           <Header title="React JS Blog" />
           <Navbar />
-          <h1>Submenu </h1>
+          <Submenu />
           <h1>Sidebar </h1>
           <Hero />
           {/* <User name="Jogata" /> */}
