87d0ea8deab459d3a59a9f693fdea6fa23a96faf
Yonko Nikolov <jonkoslav@abv.bg>
2024-10-21 17:48:18 +0300

    exercise - coctails - added context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -495,287 +495,80 @@ function useFetch(url) {
 const baseURL = "http://localhost:3030/jsonstore/blog/"
 // ============================================================
 
-const data = [
-  {
-    id: 1,
-    title: 'Samsung Galaxy S7',
-    price: 599.99,
-    img: 'https://m.media-amazon.com/images/I/61jmT4Enm1L._AC_SL1499_.jpg',
-    amount: 1,
-  },
-  {
-    id: 2,
-    title: 'google pixel ',
-    price: 499.99,
-    img: 'https://www.telepolis.pl/images/katalog/Google/Google_Pixel_6/google-pixel-6-1.jpg',
-    amount: 1,
-  },
-  {
-    id: 3,
-    title: 'Xiaomi Redmi Note 2',
-    price: 699.99,
-    img: 'https://images.tokopedia.net/img/cache/700/catalog/2019/4/29/2592140/2592140_6b615082-b36a-4bf4-96b5-6a6c6cbb84de.jpg',
-    amount: 1,
-  },
-];
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const AppContext = createContext(); 
 
-const reducer = (state, action) => {
-  if (action.type === 'CLEAR_CART') {
-    return { ...state, cart: [] };
-  }
+const AppProvider = ({ children }) => { 
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
 
-  if (action.type === 'REMOVE') {
-    return {
-      ...state,
-      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+  const fetchDrinks = useCallback( async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumb,
+            strAlcoholic,
+            strGlass,
+          } = item;
+
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          }
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      // setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
-  }
+  },[searchTerm]);
 
-  if (action.type === 'INCREASE') {
-    let tempCart = state.cart.map((cartItem) => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount + 1 };
-      }
-      return cartItem;
-    })
-    return { ...state, cart: tempCart };
-  }
+  useEffect(() => { 
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
 
-  if (action.type === 'DECREASE') {
-    let tempCart = state.cart
-      .map((cartItem) => {
-        if (cartItem.id === action.payload) {
-          return { ...cartItem, amount: cartItem.amount - 1 };
-        }
-        return cartItem;
-      })
-      .filter((cartItem) => cartItem.amount !== 0);
-    return { ...state, cart: tempCart };
-  }
-
-  if (action.type === 'GET_TOTALS') {
-    let { total, amount } = state.cart.reduce(
-      (cartTotal, cartItem) => {
-        const { price, amount } = cartItem;
-        const itemTotal = price * amount;
-
-        cartTotal.total += itemTotal;
-        cartTotal.amount += amount;
-        return cartTotal;
-      },
-      {
-        total: 0,
-        amount: 0,
-      }
-    )
-    total = parseFloat(total.toFixed(2));
-
-    return { ...state, total, amount };
-  }
-
-  if (action.type === 'LOADING') {
-    return { ...state, loading: true };
-  }
-
-  if (action.type === 'DISPLAY_ITEMS') {
-    return { ...state, cart: action.payload, loading: false };
-  }
-
-  if (action.type === 'TOGGLE_AMOUNT') {
-    let tempCart = state.cart
-      .map((cartItem) => {
-        if (cartItem.id === action.payload.id) {
-          if (action.payload.type === 'inc') {
-            return { ...cartItem, amount: cartItem.amount + 1 };
-          }
-          if (action.payload.type === 'dec') {
-            return { ...cartItem, amount: cartItem.amount - 1 };
-          }
-        }
-        return cartItem;
-      })
-      .filter((cartItem) => cartItem.amount !== 0);
-    return { ...state, cart: tempCart };
-  }
-
-  throw new Error('no matching action type');
-
-}
-
-// const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = createContext();
-
-const initialState = {
-  loading: false,
-  cart: [],
-  total: 0,
-  amount: 0,
-}
-
-const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const clearCart = () => {
-    dispatch({ type: 'CLEAR_CART' });
-  }
-
-  const remove = (id) => {
-    dispatch({ type: 'REMOVE', payload: id });
-  }
-
-  const increase = (id) => {
-    dispatch({ type: 'INCREASE', payload: id });
-  }
-
-  const decrease = (id) => {
-    dispatch({ type: 'DECREASE', payload: id });
-  }
-
-  const fetchData = async () => {
-    dispatch({ type: 'LOADING' });
-    // const response = await fetch(url);
-    // const cart = await response.json();
-    setTimeout(() => {
-      dispatch({ type: 'DISPLAY_ITEMS', payload: data });
-    }, 3000);
-  }
-
-  const toggleAmount = (id, type) => {
-    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } })
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    dispatch({ type: 'GET_TOTALS' });
-  }, [state.cart]);
-
-  return (
-    <AppContext.Provider
-      value={{
-        ...state,
-        clearCart,
-        remove,
-        increase,
-        decrease,
-        toggleAmount,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  )
-}
-
-export const useGlobalContext = () => {
-  return useContext(AppContext);
-}
-
-const Navbar = () => {
-  const { amount } = useGlobalContext()
   return (  
-    <nav>
-      <div className='nav-center'>
-        <h3>useReducer</h3>
-        <div className='nav-container'>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-            <path d='M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z' />
-          </svg>
-          <div className='amount-container'>
-            <p className='total-amount'>{amount}</p>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <AppContext.Provider  
+      value={{ loading, cocktails, searchTerm, setSearchTerm }}
+    > 
+      {children}  
+    </AppContext.Provider>  
   ) 
 } 
 
-const CartItem = ({ id, img, title, price, amount }) => {
-  const { remove, increase, decrease, toggleAmount } = useGlobalContext();
-  return (
-    <article className='cart-item'>
-      <img src={img} alt={title} />
-      <div>
-        <h4>{title}</h4>
-        <h4 className='item-price'>${price}</h4>
-        <button className='remove-btn' onClick={() => remove(id)}>
-          remove
-        </button>
-      </div>
-      <div>
-        <button className='amount-btn' onClick={() => toggleAmount(id, 'inc')}>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-            <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
-          </svg>
-        </button>
-        <p className='amount'>{amount}</p>
-        <button className='amount-btn' onClick={() => toggleAmount(id, 'dec')}>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-            <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-          </svg>
-        </button>
-      </div>
-    </article>
-  )
-}
+const useGlobalContext = () => { 
+  return useContext(AppContext);  
+} 
 
-const CartContainer = () => {
-  const { cart, total, clearCart, loading } = useGlobalContext();
-
-  if (loading) {
-    return (
-      <div className='loading'>
-        <Loader />
-      </div>
-    )
-  }
-
-  if (cart.length === 0) {
-    return (
-      <section className='cart'>
-        <header>
-          <h2>your bag</h2>
-          <h4 className='empty-cart'>is currently empty</h4>
-        </header>
-      </section>
-    )
-  }
-
-  return (
-    <section className='cart'>
-      <header>
-        <h2>your bag</h2>
-      </header>
-      <div>
-        {cart.map((item) => {
-          return <CartItem key={item.id} {...item} />
-        })}
-      </div>
-      <footer>
-        <hr />
-        <div className='cart-total'>
-          <h4>
-            total <span>${total}</span>
-          </h4>
-        </div>
-        <button className='btn clear-btn' onClick={clearCart}>
-          clear cart
-        </button>
-      </footer>
-    </section>
-  )
+const Test = () => {
+  const { loading } = useGlobalContext();
+  return loading ? <Loader /> : null;
 }
 
 function App() {
   return (
     <>
-      {/* <AuthContextProvider> */}
-      <AppProvider>
+      {/* <AuthContextProvider> */} 
+      <AppProvider>  
         <div className="body">
           <Header title="React JS Blog" />
-          <Navbar />
-          <CartContainer />
+          <Test />
           {/* <User name="Jogata" /> */}
           {/* <User render={(isLoggedIn) => isLoggedIn ? "Jogata" : "Guest" } /> */}
           {/* <DataProvider>
@@ -825,8 +618,8 @@ function App() {
           </svg>
 
         </div>
-      {/* </AuthContextProvider> */}
-      </AppProvider>
+      {/* </AuthContextProvider> */}  
+      </AppProvider> 
     </>
   )
 }
