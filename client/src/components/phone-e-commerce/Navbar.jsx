97c9736d820567862a41b5008613564a48bce853
Yonko Nikolov <jonkoslav@abv.bg>
2024-11-11 09:51:28 +0200

    added nav component

--- client/src/components/phone-e-commerce/Navbar.jsx
+++ client/src/components/phone-e-commerce/Navbar.jsx
@@ -1,12 +1,28 @@
+import { Link } from "react-router-dom"
 import logo from "./assets/logo.svg"
 
 export function Navbar() {
     return (
-        <>
-            <a href="#">
+        <nav>
+            {/* <h2 className="blue-text">navbar component</h2> */}
+            <Link to="/" className="logo">
+                {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+                    Creative Commons (Attribution 3.0 Unported);
+                    https://www.iconfinder.com/Makoto_msk 
+                */}
                 <img src={logo} />
-            </a>
-            <h2 className="blue-text">navbar component</h2>
-        </>
+            </Link>
+            <ul>
+                <li>
+                    <Link to="/">Products</Link>
+                </li>
+                <li className="cart-link">
+                    <Link to="/cart">
+                        <i className="fa fa-shopping-cart"></i>
+                        my cart
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
