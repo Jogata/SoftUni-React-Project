6003841d8f69a89f524b0abc5c233f28366a5aae
Yonko Nikolov <jonkoslav@abv.bg>
2024-11-16 22:38:00 +0200

    set up cart colums component

--- client/src/components/phone-e-commerce/Cart/Cart.jsx
+++ client/src/components/phone-e-commerce/Cart/Cart.jsx
@@ -1,11 +1,13 @@
-import { ProductConsumer } from "./Context";
+import { ProductConsumer } from "../Context";
 import { Title } from "../Title";
+import { CartColums } from "./CartColumns";
 
 export function Cart() {
     return (
         <section>
             {/* <h2 className="blue-text">Cart component</h2> */}
             <Title name={"Your"} title={"Cart"} />
+            <CartColums />
         </section>
     )
 }
