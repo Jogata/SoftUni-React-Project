cbb702396119581aa9414f0160b6eb1dc70a7a14
Yonko Nikolov <jonkoslav@abv.bg>
2024-11-27 12:17:37 +0200

    updated single room component

--- client/src/components/beach-resort/components/SingleRoom.jsx
+++ client/src/components/beach-resort/components/SingleRoom.jsx
@@ -10,7 +10,7 @@ export function SingleRoom() {
     // console.log(slug);
     const { getSingleRoom } = useContext(RoomContext);
 
-    const [ room, setRoom ] = useState({name: "test", images: ["none"]});
+    const [ room, setRoom ] = useState({name: "test", images: ["none"], description: "", price: 0, size: 0, capacity: 0, pets: false, breakfast: false, extras: []});
     // console.log(room);
     useEffect(() => {
         // console.log("effect");
@@ -26,6 +26,37 @@ export function SingleRoom() {
                     <Link to="/rooms" className="btn-primary">back to rooms</Link>
                 </Banner>
             </Hero>
+            <section className="single-room">
+                <div className="single-room-images">
+                    {room.images.map((url, index) => {
+                        return <img key={index} src={url} alt={room.name} />
+                    })}
+                </div>
+                <div className="single-room-info">
+                    <article className="desc">
+                        <h3>details</h3>
+                        <p>{room.description}</p>
+                    </article>
+                    <article className="info">
+                        <h3>info</h3>
+                        <h6>price: ${room.price}</h6>
+                        <h6>size: {room.size} SQFT</h6>
+                        <h6>max capacity: {room.capacity} {room.capacity > 1 ? "people" : "person"} </h6>
+                        <h6>{room.pets ? "pets allowed" : "no pets allowed"}</h6>
+                        <h6>{room.breakfast && "free breakfast included"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className="room-extras">
+                <h6>extras</h6>
+                <ul className="extras">
+                    {room.extras.map((item, index) => {
+                        return (
+                            <li key={index}>{item}</li>
+                        )
+                    })}
+                </ul>
+            </section>
         </div>
     )
 }
