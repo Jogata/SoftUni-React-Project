c9b7934f0e310d7eeb75683b8ad4f2f8b1ed4d89
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-02 17:28:43 +0200

    practice

--- client/src/components/beach-resort/components/ExerciseOne.jsx
+++ client/src/components/beach-resort/components/ExerciseOne.jsx
@@ -1,30 +1,37 @@
 export function ExerciseOne() {
     return (
         <div className="e-one full-screen">
+            <header>
+                <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/01.%20Static%20pages/11.%20Random%20housekeeping/react-logo.png" alt="" />
+                <span>React</span>
+            </header>
             <h1>Why i want to learn React?</h1>
             <ol>
-                <li className="bolled">
-                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed accusantium maiores vitae adipisci placeat saepe, neque quis ad at perspiciatis tempora sapiente eius doloremque ea, tenetur quia unde mollitia commodi aliquam laboriosam doloribus. Maiores similique fugit itaque assumenda quia laboriosam magni nemo nesciunt, commodi doloremque quidem veniam aliquam nulla laudantium exercitationem, minus sapiente provident quasi?</span>
+                <li className="numbred">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed accusantium maiores vitae adipisci placeat saepe, neque quis ad at perspiciatis tempora sapiente eius doloremque ea, tenetur quia unde mollitia commodi aliquam laboriosam doloribus. Maiores similique fugit itaque assumenda quia laboriosam magni nemo nesciunt, commodi doloremque quidem veniam aliquam nulla laudantium exercitationem, minus sapiente provident quasi?</p>
                 </li>
-                <li className="bulled">
+                <li className="numbred">
                     By learning React, you enhance your chances of landing a job or 
                     advancing your career in the web development field.
                 </li>
-                <li className="bulled">
+                <li className="numbred">
                     React can be used to create any type of web application.
                 </li>
-                <li className="bulled">
+                <li className="numbred">
                     Strong job prospects for React developers, with 
                     attractive salaries and career growth opportunities.
                 </li>
-                <li className="bulled">
+                <li className="numbred">
                     React is ideal for web and UI developers to develop 
                     highly responsive, beautiful components.
                 </li>
-                <li className="bulled">
+                <li className="numbred">
                     React is a very declarative, fast, and flexible JavaScript library.
                 </li>
             </ol>
+            <footer>
+                <p>&copy; 2024. All right reserved.</p>
+            </footer>
         </div>
     )
 }
