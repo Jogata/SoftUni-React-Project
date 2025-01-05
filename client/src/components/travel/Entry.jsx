d4efe17b6c080df869bd1b4ec583dac8aa869cc1
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-05 17:17:53 +0200

    added css for single journal article component

--- client/src/components/travel/Entry.jsx
+++ client/src/components/travel/Entry.jsx
@@ -1,18 +1,18 @@
 export function Entry() {
     return (
         <article className="journal">
-            <img src="" alt="add image" />
-            <div>
                 <div className="journal-image-box">
-                <img src="" alt="marker icon" className="journal-image" />
+                    <img className="journal-image" src="https://raw.githubusercontent.com/Jogata/CSS/refs/heads/main/assets/img/stock-photo-131318185-copy.jpg" alt="add image" />
                 </div>
-                <span>Japan</span>
+            <div className="journal-info">
+                <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
+                <span className="country">Japan</span>
                 <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-                    Google Maps Link
+                    View on the map
                 </a>
-                <h2>Mount Fuji</h2>
-                <p>12 Jan, 2021 - 24 Jan, 2021</p>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h2 className="entry-title">Mount Fuji</h2>
+                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </article>
     )
