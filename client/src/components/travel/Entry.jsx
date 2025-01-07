fe6e4c2c87815bb4ca8b327ab6251ce2d5080afe
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-07 18:59:57 +0200

    refactoring journal component with props

--- client/src/components/travel/Entry.jsx
+++ client/src/components/travel/Entry.jsx
@@ -1,42 +1,30 @@
 export function Entry(props) {
-    // console.log(props);
-
-    return (
-        <article className="journal">
-                <div className="journal-image-box">
-                    <img className="journal-image" src="https://raw.githubusercontent.com/Jogata/CSS/refs/heads/main/assets/img/stock-photo-131318185-copy.jpg" alt="add image" />
-                </div>
-            <div className="journal-info">
-                <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
-                <span className="country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-                    View on the map
-                </a>
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
-            </div>
-        </article>
-    )
-}
-
-export function Joke(props) {
     console.log(props);
 
     return (
-        <>
-            <article className="joke">
-                {/* <h1>joke comp</h1> */}
-                {props.setup &&
-                    <p className="setup">
-                        {props.setup}
-                    </p>
-                }
-                <p className="punchline">
-                    {props.punchline}
+        <article className="journal">
+            <div className="journal-image-box">
+                <img
+                    className="journal-image"
+                    src={props.src}
+                    alt="image" />
+            </div>
+            <div className="journal-info">
+                <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
+                <span className="country">{props.country}</span>
+                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+                    View on the map
+                </a>
+                <h2 className="entry-title">
+                    {props.title}
+                </h2>
+                <p className="trip-dates">
+                    {props.dates}
                 </p>
-                <hr />
-            </article>
-        </>
+                <p className="entry-text">
+                    {props.text}
+                </p>
+            </div>
+        </article>
     )
 }
--- client/src/components/travel/Main.jsx
+++ client/src/components/travel/Main.jsx
@@ -1,148 +1,16 @@
-import { Entry, Joke } from "./Entry";
+import { Entry } from "./Entry";
 
 export function Main() {
     return (
         <main>
             <h1>main component</h1>
-            {/* <Entry /> */}
-
-            <Joke
-                // setup="I got my daughter a fridge for her birthday."
-                punchline="I can't wait to see her face light up when she opens it."
+            <Entry
+                src="https://raw.githubusercontent.com/Jogata/CSS/refs/heads/main/assets/img/stock-photo-131318185-copy.jpg"
+                country="Japan"
+                title="Mount Fuji"
+                dates="12 Jan, 2021 - 24 Jan, 2021"
+                text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
             />
-            <Joke
-                setup="How did the hacker escape the police?"
-                punchline="He just ransomware!"
-            />
-            <Joke
-                setup="Why don't pirates travel on mountain roads?"
-                punchline="Scurvy."
-            />
-            <Joke
-                setup="Why do bees stay in the hive in the winter?"
-                punchline="Swarm."
-            />
-            <Joke
-                setup="What's the best thing about Switzerland?"
-                punchline="I don't know, but the flag is a big plus!"
-            />
-
-            <div className="contacts" style={{display: "none"}}>
-                <Entry 
-                    img="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/mr-whiskerson.png" 
-                    name="Mr. Whiskerson" 
-                    phone="(212) 555-1234" 
-                    email="mr.whiskaz@catnap.meow"
-                />
-                <Entry 
-                    img="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/fluffykins.png" 
-                    name="Fluffykins" 
-                    phone="(212) 555-2345" 
-                    email="fluff@me.com"
-                />
-                <Entry 
-                    img="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/felix.png" 
-                    name="Felix" 
-                    phone="(212) 555-4567" 
-                    email="thecat@hotmail.com"
-                />
-                <Entry 
-                    img="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/pumpkin.png" 
-                    name="Pumpkin" 
-                    phone="(0800) CAT KING" 
-                    email="pumpkin@scrimba.com"
-                />
-
-                {/* <article className="contact-card">
-                    <img
-                        src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/mr-whiskerson.png"
-                        alt="Photo of Mr. Whiskerson"
-                    />
-                    <h3>Mr. Whiskerson</h3>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/phone-icon.png"
-                            alt="phone icon"
-                        />
-                        <p>(212) 555-1234</p>
-                    </div>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/mail-icon.png"
-                            alt="mail icon"
-                        />
-                        <p>mr.whiskaz@catnap.meow</p>
-                    </div>
-                </article>
-
-                <article className="contact-card">
-                    <img
-                        src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/fluffykins.png"
-                        alt="Photo of Fluffykins"
-                    />
-                    <h3>Fluffykins</h3>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/phone-icon.png"
-                            alt="phone icon"
-                        />
-                        <p>(212) 555-2345</p>
-                    </div>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/mail-icon.png"
-                            alt="mail icon"
-                        />
-                        <p>fluff@me.com</p>
-                    </div>
-                </article>
-
-                <article className="contact-card">
-                    <img
-                        src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/felix.png"
-                        alt="Photo of Felix"
-                    />
-                    <h3>Felix</h3>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/phone-icon.png"
-                            alt="phone icon"
-                        />
-                        <p>(212) 555-4567</p>
-                    </div>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/mail-icon.png"
-                            alt="mail icon"
-                        />
-                        <p>thecat@hotmail.com</p>
-                    </div>
-                </article>
-
-                <article className="contact-card">
-                    <img
-                        src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/pumpkin.png"
-                        alt="Photo of Pumpkin"
-                    />
-                    <h3>Pumpkin</h3>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/phone-icon.png"
-                            alt="phone icon"
-                        />
-                        <p>(0800) CAT KING</p>
-                    </div>
-                    <div className="info-group">
-                        <img
-                            src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/08.%20Props%20Part%203%20-%20Create%20a%20contact%20component/images/mail-icon.png"
-                            alt="mail icon"
-                        />
-                        <p>pumpkin@scrimba.com</p>
-                    </div>
-                </article> */}
-
-            </div>
-
         </main>
     )
 }
