c86ee2a4f1152d61b3e34382d3d7e018e5bae7d5
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-08 18:05:47 +0200

    added google maps link to journal component props

--- client/src/components/travel/Entry.jsx
+++ client/src/components/travel/Entry.jsx
@@ -14,7 +14,7 @@ export function Entry(props) {
             <div className="journal-info">
                 <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
                 <span className="country">{props.country}</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+                <a href={props.googleMapsLink}>
                     View on the map
                 </a>
                 <h2 className="entry-title">
