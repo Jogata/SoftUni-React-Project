789281ec31b3891f63a217c7d219334a8f4d650b
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-10 12:24:33 +0200

    generate jokes with array map

--- client/src/components/travel/Entry.jsx
+++ client/src/components/travel/Entry.jsx
@@ -2,32 +2,37 @@ export function Entry(props) {
     console.log(props);
 
     return (
-        <article className="journal">
-            <div className="journal-image-box">
-                <img
-                    className="journal-image"
-                    src={props.img.src}
-                    // alt="image" 
-                    alt={props.img.alt}
-                />
-            </div>
-            <div className="journal-info">
-                <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink}>
-                    View on the map
-                </a>
-                <h2 className="entry-title">
-                    {props.title}
-                </h2>
-                <p className="trip-dates">
-                    {props.dates}
-                </p>
-                <p className="entry-text">
-                    {props.text}
-                </p>
-            </div>
-        </article>
+        // <article className="journal">
+        //     <div className="journal-image-box">
+        //         <img
+        //             className="journal-image"
+        //             src={props.img.src}
+        //             // alt="image" 
+        //             alt={props.img.alt}
+        //         />
+        //     </div>
+        //     <div className="journal-info">
+        //         <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
+        //         <span className="country">{props.country}</span>
+        //         <a href={props.googleMapsLink}>
+        //             View on the map
+        //         </a>
+        //         <h2 className="entry-title">
+        //             {props.title}
+        //         </h2>
+        //         <p className="trip-dates">
+        //             {props.dates}
+        //         </p>
+        //         <p className="entry-text">
+        //             {props.text}
+        //         </p>
+        //     </div>
+        // </article>
+        <>
+            {props.setup && <p className="setup">Setup: {props.setup}</p>}
+            <p className="punchline">Punchline: {props.punchline}</p>
+            <hr />
+        </>
     )
 }
 
--- client/src/components/travel/Main.jsx
+++ client/src/components/travel/Main.jsx
@@ -1,10 +1,15 @@
-import { Entry } from "./Entry";
+import { Entry, jokesData } from "./Entry";
 
 export function Main() {
+    const jokeElements = jokesData.map((joke) => {
+        return <Entry setup={joke.setup} punchline={joke.punchline} />
+    });
+
     return (
         <main>
             <h1>main component</h1>
-            <Entry
+            {jokeElements}
+            {/* <Entry
                 img={{
                     src: "https://raw.githubusercontent.com/Jogata/CSS/refs/heads/main/assets/img/stock-photo-131318185-copy.jpg", 
                     alt: "Mount Fuji"
@@ -14,7 +19,8 @@ export function Main() {
                 googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" 
                 dates="12 Jan, 2021 - 24 Jan, 2021" 
                 text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-            />
+            /> */}
+
         </main>
     )
 }
