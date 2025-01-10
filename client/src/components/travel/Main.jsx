594d73426bd397c7b13c026582937252396f30f2
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-10 19:46:07 +0200

    generate travel journal with array map

--- client/src/components/travel/Main.jsx
+++ client/src/components/travel/Main.jsx
@@ -1,10 +1,26 @@
-import { Entry } from "./Entry";
+import { Entry, data } from "./Entry";
 
 export function Main() {
+
+    const entries = data.map(entry => {
+        return (
+            <Entry 
+                key={entry.id}
+                img={entry.img} 
+                title={entry.title} 
+                country={entry.country} 
+                googleMapsLink={entry.googleMapsLink}
+                dates={entry.dates} 
+                text={entry.text}
+            />
+        )
+    });
+
     return (
         <main>
             {/* <h1>main component</h1> */}
-            <Entry
+            {entries}
+            {/* <Entry
                 img={{
                     src: "https://raw.githubusercontent.com/Jogata/CSS/refs/heads/main/assets/img/stock-photo-131318185-copy.jpg", 
                     alt: "Mount Fuji"
@@ -14,7 +30,7 @@ export function Main() {
                 googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" 
                 dates="12 Jan, 2021 - 24 Jan, 2021" 
                 text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-            />
+            /> */}
         </main>
     )
 }
