9b9ad2b4b5cd04ddf98b3c53770ec0ebb0433b89
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-10 16:09:17 +0200

    added travels data

--- client/src/components/travel/Entry.jsx
+++ client/src/components/travel/Entry.jsx
@@ -2,59 +2,70 @@ export function Entry(props) {
     console.log(props);
 
     return (
-        // <article className="journal">
-        //     <div className="journal-image-box">
-        //         <img
-        //             className="journal-image"
-        //             src={props.img.src}
-        //             // alt="image" 
-        //             alt={props.img.alt}
-        //         />
-        //     </div>
-        //     <div className="journal-info">
-        //         <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
-        //         <span className="country">{props.country}</span>
-        //         <a href={props.googleMapsLink}>
-        //             View on the map
-        //         </a>
-        //         <h2 className="entry-title">
-        //             {props.title}
-        //         </h2>
-        //         <p className="trip-dates">
-        //             {props.dates}
-        //         </p>
-        //         <p className="entry-text">
-        //             {props.text}
-        //         </p>
-        //     </div>
-        // </article>
-        <>
-            {props.setup && <p className="setup">Setup: {props.setup}</p>}
-            <p className="punchline">Punchline: {props.punchline}</p>
-            <hr />
-        </>
+        <article className="journal">
+            <div className="journal-image-box">
+                <img
+                    className="journal-image"
+                    src={props.img.src}
+                    // alt="image" 
+                    alt={props.img.alt}
+                />
+            </div>
+            <div className="journal-info">
+                <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/02.%20Data-Driven%20React/04.%20Problem%20-%20Not%20reusable/images/marker.png" alt="marker icon" className="journal-icon" />
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink}>
+                    View on the map
+                </a>
+                <h2 className="entry-title">
+                    {props.title}
+                </h2>
+                <p className="trip-dates">
+                    {props.dates}
+                </p>
+                <p className="entry-text">
+                    {props.text}
+                </p>
+            </div>
+        </article>
     )
 }
 
-export const jokesData = [
+export const data = [
     {
-        setup: "I got my daughter a fridge for her birthday.",
-        punchline: "I can't wait to see her face light up when she opens it."
+        id: 1,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
     },
     {
-        setup: "How did the hacker escape the police?",
-        punchline: "He just ransomware!"
+        id: 2,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            alt: "Sydney Opera House"
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
     },
     {
-        setup: "Why don't pirates travel on mountain roads?",
-        punchline: "Scurvy."
+        id: 3,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-norway-image-url",
+            alt: "Geirangerfjord"
+        },
+        title: "Geirangerfjord",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
-    {
-        setup: "Why do bees stay in the hive in the winter?",
-        punchline: "Swarm."
-    },
-    {
-        setup: "What's the best thing about Switzerland?",
-        punchline: "I don't know, but the flag is a big plus!"
-    }
 ]
