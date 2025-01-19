4908afad24dbb9ae7fe34e49cf021f68f0a92c3e
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-19 14:52:12 +0200

    practice forms

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -16,83 +16,28 @@ export function Main() {
         setIngridients(prevIngridients => [...prevIngridients, newIngridient]);
     }
 
-    const avatar = "https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/03.%20React%20State/19.%20Complex%20state%20-%20Objects/images/user.png";
-    const starEmpty = "https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/03.%20React%20State/19.%20Complex%20state%20-%20Objects/images/star-empty.png";
-    const starFilled = "https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/03.%20React%20State/19.%20Complex%20state%20-%20Objects/images/star-filled.png";
-
-    const [contact, setContact] = useState({
-        firstName: "John", 
-        lastName: "Doe", 
-        phone: "+1 (212) 555-1212", 
-        email: "itsmyrealname@example.com", 
-        isFavorite: false
-    })
-
-    const starIcon = contact.isFavorite ? starFilled : starEmpty;
-
-    function toggleFavorite() {
-        // console.log("Toggle Favorite");
-        setContact(prevContact => {
-            return {
-                ...prevContact, 
-                isFavorite: !prevContact.isFavorite
-            }
-        });
+    function signUp(formData) {
+        const email = formData.get("email");
+        console.log(email);
+        const password = formData.get("password");
+        console.log(password);
     }
-
-    function submit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const firstName = formData.get("firstName");
-        const lastName = formData.get("lastName");
-        submitViaAPI({
-            firstName,
-            lastName
-        });
-    }
-    
-    function submitViaAPI(data) {
-        console.log(data);
-        console.log("Submitted!");
-    }
-        
     return (
         <main>
-            <form method="POST" id="my-form" onSubmit={submit}>
-                <label htmlFor="first-name">First Name: </label>
-                <input type="text" id="first-name" name="firstName" className="test-input" />
-                <br />
-                <label htmlFor="last-name">Last Name: </label>
-                <input type="text" id="last-name" name="lastName" className="test-input" />
-                <br />
-                <input type="submit" value="Submit" />
-            </form>
-            <article className="card" style={{display: "none"}}>
-                <img
-                    src={avatar}
-                    className="avatar"
-                    alt="User profile picture of John Doe"
-                />
-                <div className="info">
-                    <button
-                        onClick={toggleFavorite} 
-                        aria-pressed={contact.isFavorite} 
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"} 
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
-                    <h2 className="name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="contact">{contact.phone}</p>
-                    <p className="contact">{contact.email}</p>
-                </div>
-            </article>
+            <section>
+                <h1>Signup form</h1>
+                {/* <form action={signUp}> */}
+                <form onSubmit={e => signUp(new FormData(e.currentTarget))}>
+                    <label htmlFor="email">Email:</label>
+                    <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+                    <br />
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" type="password" name="password" />
+                    <br />
+                    <button>Submit</button>
+                </form>
+            </section>
+    
             <form className="recipe-form" onSubmit={handleSubmit} style={{display: "none"}}>
                 <input 
                     type="text" 
