a0b7e743bace26721f16a5641b38ad5545bd16e5
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-22 18:13:37 +0200

    added list of ingridients component

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export function Main() {
-    const [ingridients, setIngridients] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
     const [value, setValue] = useState("");
 
-    const ingridientsList = ingridients.map(ingridient => (
-        <li key={ingridient}>{ingridient}</li>
+    const ingredientsList = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
     ))
 
     function handleSubmit(event) {
@@ -14,22 +14,9 @@ export function Main() {
         const formData = new FormData(form);
         // const data = Object.fromEntries(formData);
         // console.log(data);
-        // setIngridients(prevIngridients => [...prevIngridients, newIngridient]);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
         form.reset();
-    }
-
-    function Messages() {
-        const [messages, setMessages] = useState(["a", "b"]);
-
-        return (
-            <ol className="messages">
-                {messages.map((m, i) => {
-                    return (
-                        <li key={i}>{m}</li>
-                    )
-                })}
-            </ol>
-        )
     }
 
     function handleChange(e) {
@@ -39,24 +26,36 @@ export function Main() {
 
     return (
         <main>
-            <Messages />
-            <form className="recipe-form" onSubmit={handleSubmit} style={{display: "none"}}>
+            <form className="recipe-form" onSubmit={handleSubmit}>
                 <input 
                     className="value"
                     type="text" 
-                    name="ingridient" 
+                    name="ingredient" 
                     placeholder="e.g. oregano" 
-                    aria-label="add ingridient" 
+                    aria-label="add ingredient" 
                     onChange={e => handleChange(e)} 
                     value={value}
                 />
 
-                <button type="submit">Add ingridient</button>
+                <button type="submit">Add ingredient</button>
 
             </form>
-            <ul>
-                {ingridientsList}
-            </ul>
+            {/* <ul>
+                {ingredientsList}
+            </ul> */}
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">
+                    {ingredientsList}
+                </ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>}
         </main>
     )
 }
