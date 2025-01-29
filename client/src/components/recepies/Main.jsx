88a426421a9f02bc95c4ce7fdbb8767a0064ec19
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-29 15:59:30 +0200

    practice

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -7,6 +7,18 @@ export function Main() {
         src: "http://i.imgflip.com/1bij.jpg"
     });
 
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        // .then(obj => console.log(obj.data.memes));
+        .then(obj => setState(prev => {
+            return {
+                ...prev, 
+                src: obj.data.memes[1].url
+            }
+        }));
+    }, []);
+
     function updateState(e) {
         const property = e.currentTarget.name;
         const value = e.currentTarget.value;
@@ -17,43 +29,8 @@ export function Main() {
         // console.log(state);
     }
 
-    const [data, setData] = useState(null);
-    const [id, setId] = useState(1);
-
-    useEffect(() => {
-        console.log("Effect ran");
-        fetch(`https://swapi.dev/api/people/${id}`)
-            .then(res => res.json())
-            // .then(data => console.log(data));
-            .then(data => setData(data));
-    }, [id]);
-
-    console.log("Rendered!");
-
-    function Next() {
-        setId(prev => prev + 1);
-    }
-
-    function Prev() {
-        setId(prev => prev - 1);
-    }
-
-    // const temp1 = document.createElement("div");
-    // const temp0 = document.createElement("button");
-    // temp1.addEventListener("click", function () {
-    //     setTimeout(() => temp0.style.transform("scale(0)"), 0);
-    // })
-
     return (
         <main>
-            <div>
-                <pre>
-                    {JSON.stringify(data, null, 2)}
-                </pre>
-            </div>
-            <button className="btn" onClick={Next}>Next</button>
-            <button className="btn" onClick={Prev}>Prev</button>
-            <span id="count">{id}</span>
             <div className="form">
                 <label>Top Text
                     <input
