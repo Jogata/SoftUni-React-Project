148b702a7bab898d75a87c971bc10b0f60109ed1
Yonko Nikolov <jonkoslav@abv.bg>
2025-01-31 15:11:32 +0200

    set up tenzies game project

--- client/src/components/recepies/Header.jsx
+++ client/src/components/recepies/Header.jsx
@@ -1,20 +1,5 @@
 export function Header() {
     return (
-        <>
-            <header className="header">
-                <div className="logo">
-                    <h4>Logo</h4>
-                    {/* <img src={chefClaudeLogo} /> */}
-                    <h1>Chef Claude</h1>
-                </div>
-            </header>
-        <header className="header" style={{display: "none"}}>
-            <div className="logo">
-                <h4>Logo</h4>
-                {/* <img src="https://raw.githubusercontent.com/scrimba/learn-react/refs/heads/main/04.%20Side%20Effects/02.%20Meme%20Generator%20Starting%20Point/images/troll-face.png" alt="" /> */}
-                <h1>Meme Generator</h1>
-            </div>
-        </header>
-        </>
+        <h1>header</h1>
     )
 }
