1f43490b4c1ac944720fcd3f3c5da29b280bad5f
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-01 13:45:19 +0200

    set up tenzies game project

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -13,29 +13,23 @@ export function Main() {
             const number = Math.ceil(Math.random() * max);
             arr[index] = number;
         }
-        // console.log(arr);
         return arr;
     }
 
     console.log(dice);
-    // generateArrayOfNumbers(10, 6);
     const buttons = dice.map((number, index) => {
         return <Die key={index} value={number} />
     })
 
+    function roll() {
+        const newDice = generateArrayOfNumbers(10, 6);
+        setDice(newDice);
+    }
+
     return (
         <main id="tenzies">
             {buttons}
-            {/* <Die value={1} />
-            <Die value={2} />
-            <Die value={3} />
-            <Die value={4} />
-            <Die value={5} />
-            <Die value={6} />
-            <Die value={1} />
-            <Die value={2} />
-            <Die value={3} />
-            <Die value={4} /> */}
+            <button className="roll" onClick={roll}>roll</button>
         </main>
     )
 }
