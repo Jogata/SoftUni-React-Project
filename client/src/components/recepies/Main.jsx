8a3fa2c85c51c732116ddf185ef75bb985c16349
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-01 13:36:39 +0200

    set up tenzies game project

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 function Die(props) {
     return <button>{props.value}</button>
 }
 
 export function Main() {
+    const [dice, setDice] = useState(generateArrayOfNumbers(10, 6));
+
+    function generateArrayOfNumbers(length, max) {
+        const arr = new Array(length);
+        for (let index = 0; index < arr.length; index++) {
+            const number = Math.ceil(Math.random() * max);
+            arr[index] = number;
+        }
+        // console.log(arr);
+        return arr;
+    }
+
+    console.log(dice);
+    // generateArrayOfNumbers(10, 6);
+    const buttons = dice.map((number, index) => {
+        return <Die key={index} value={number} />
+    })
+
     return (
         <main id="tenzies">
-            <Die value={1} />
+            {buttons}
+            {/* <Die value={1} />
             <Die value={2} />
             <Die value={3} />
             <Die value={4} />
@@ -14,7 +35,7 @@ export function Main() {
             <Die value={1} />
             <Die value={2} />
             <Die value={3} />
-            <Die value={4} />
+            <Die value={4} /> */}
         </main>
     )
 }
