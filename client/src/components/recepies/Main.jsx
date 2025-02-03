f5291f336a76601663aeab19de1e7eceb0831477
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-03 20:56:05 +0200

    set up tenzies game project

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -15,36 +15,40 @@ function Die(props) {
 }
 
 export function Main() {
-    const [dice, setDice] = useState(generateArrayOfNumbers(10, 6));
-    let buttonText = "Roll";
+    const [dice, setDice] = useState(() => generateArrayOfNumbers(10, 6));
+    // let buttonText = "Roll";
 
-    let isGameOver = false;
-    let callback = () => roll(6);
+    // let isGameOver = false;
+    // let callback = () => roll(6);
 
-    isGameOver = dice.every(x => x.isHeld === true);
+    const gameWon = dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value);
+
+    // isGameOver = dice.every(x => x.isHeld === true);
     // console.log(isGameOver);
-    if (isGameOver) {        
-        isGameOver = checkEN(dice);
+    // if (isGameOver) {
+        // isGameOver = checkEN(dice);
         // console.log(isGameOver);
-        buttonText = "New Game";
-        callback = () => startNewGame(10, 6);
-    }
+        // buttonText = "New Game";
+        // callback = () => startNewGame(10, 6);
+    // }
 
-    function checkEN(arr) {
+    // function checkEN(arr) {
         // console.log(arr);
-        let areEqual = true;
-        const first = arr[0].value;
-        for (let index = 1; index < arr.length; index++) {
-            if (arr[index].value !== first) {
+        // let areEqual = true;
+        // const first = arr[0].value;
+        // for (let index = 1; index < arr.length; index++) {
+            // if (arr[index].value !== first) {
                 // console.log(first);
                 // console.log("are diff");
-                areEqual = false;
-            }
-        }
-        return areEqual;
-    }
+                // areEqual = false;
+            // }
+        // }
+        // return areEqual;
+    // }W
 
     function generateArrayOfNumbers(length, max) {
+        console.log("generate");
         const arr = new Array(length);
         for (let index = 0; index < arr.length; index++) {
             const number = Math.ceil(Math.random() * max);
@@ -58,7 +62,7 @@ export function Main() {
         return arr;
     }
 
-    // console.log(dice);
+    console.log(dice);
     const buttons = dice.map(obj => {
         return (
             <Die 
@@ -92,7 +96,7 @@ export function Main() {
                 const oldValue = dice.value;
                 let number = Math.ceil(Math.random() * max);
                 if (number == oldValue) {
-                    // console.log(`${number} == ${oldValue}`);
+                    console.log(`${number} == ${oldValue}`);
                     number = number + oldValue;
                     number = number % max || Math.ceil(Math.random() * max);
                 }
@@ -113,8 +117,11 @@ export function Main() {
             <div className="container">
                 {buttons}
             </div>
+            <button className="roll" onClick={() => roll(6)}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
             {/* <button className="roll" onClick={() => roll(6)}>roll</button> */}
-            <button className="roll" onClick={callback}>{buttonText}</button>
+            {/* <button className="roll" onClick={callback}>{buttonText}</button> */}
             {/* {isGameOver && <button className="roll" onClick={() => startNewGame(10, 6)}>New Game</button>} */}
         </main>
     )
