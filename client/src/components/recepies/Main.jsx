b4bfc40b3b0662451b403f9d9657dd4319598df9
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-03 17:46:25 +0200

    set up tenzies game project

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function Die(props) {
-    console.log(props.id);
+    // console.log(props.id);
     // console.log(props.isHeld);
     const classes = props.isHeld ? "green" : "white";
     return (
@@ -16,6 +16,33 @@ function Die(props) {
 
 export function Main() {
     const [dice, setDice] = useState(generateArrayOfNumbers(10, 6));
+    let buttonText = "Roll";
+
+    let isGameOver = false;
+    let callback = () => roll(6);
+
+    isGameOver = dice.every(x => x.isHeld === true);
+    // console.log(isGameOver);
+    if (isGameOver) {        
+        isGameOver = checkEN(dice);
+        // console.log(isGameOver);
+        buttonText = "New Game";
+        callback = () => startNewGame(10, 6);
+    }
+
+    function checkEN(arr) {
+        // console.log(arr);
+        let areEqual = true;
+        const first = arr[0].value;
+        for (let index = 1; index < arr.length; index++) {
+            if (arr[index].value !== first) {
+                // console.log(first);
+                // console.log("are diff");
+                areEqual = false;
+            }
+        }
+        return areEqual;
+    }
 
     function generateArrayOfNumbers(length, max) {
         const arr = new Array(length);
@@ -44,8 +71,8 @@ export function Main() {
         )
     })
 
-    function startNewGame() {
-        const newDice = generateArrayOfNumbers(10, 6);
+    function startNewGame(number, max) {
+        const newDice = generateArrayOfNumbers(number, max);
         setDice(newDice);
     }
 
@@ -86,7 +113,9 @@ export function Main() {
             <div className="container">
                 {buttons}
             </div>
-            <button className="roll" onClick={() => roll(6)}>roll</button>
+            {/* <button className="roll" onClick={() => roll(6)}>roll</button> */}
+            <button className="roll" onClick={callback}>{buttonText}</button>
+            {/* {isGameOver && <button className="roll" onClick={() => startNewGame(10, 6)}>New Game</button>} */}
         </main>
     )
 }
