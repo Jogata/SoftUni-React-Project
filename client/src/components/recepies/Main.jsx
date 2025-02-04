517776550ba0884156c9c24786b3e02b7378255e
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-04 12:01:54 +0200

    set up tenzies game project

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Die(props) {
     // console.log(props.id);
@@ -6,8 +6,11 @@ function Die(props) {
     const classes = props.isHeld ? "green" : "white";
     return (
         <button 
-        className={classes}
+            className={classes} 
             onClick={() => props.toggle(props.id)} 
+            aria-pressed={props.isHeld} 
+            aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
         >
             {props.value}
         </button>
@@ -16,39 +19,47 @@ function Die(props) {
 
 export function Main() {
     const [dice, setDice] = useState(() => generateArrayOfNumbers(10, 6));
-    // let buttonText = "Roll";
+    const buttonRef = useRef(null);
+    let buttonText = "Roll";
 
-    // let isGameOver = false;
-    // let callback = () => roll(6);
+    let isGameOver = false;
+    let callback = () => roll(6);
 
-    const gameWon = dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value);
+    // const gameWon = dice.every(die => die.isHeld) &&
+    //     dice.every(die => die.value === dice[0].value);
 
-    // isGameOver = dice.every(x => x.isHeld === true);
+    isGameOver = dice.every(x => x.isHeld === true);
     // console.log(isGameOver);
-    // if (isGameOver) {
-        // isGameOver = checkEN(dice);
+    if (isGameOver) {
+        isGameOver = checkEN(dice);
         // console.log(isGameOver);
-        // buttonText = "New Game";
-        // callback = () => startNewGame(10, 6);
-    // }
+        buttonText = "New Game";
+        callback = () => startNewGame(10, 6);
+    }
 
-    // function checkEN(arr) {
+    function checkEN(arr) {
         // console.log(arr);
-        // let areEqual = true;
-        // const first = arr[0].value;
-        // for (let index = 1; index < arr.length; index++) {
-            // if (arr[index].value !== first) {
+        let areEqual = true;
+        const first = arr[0].value;
+        for (let index = 1; index < arr.length; index++) {
+            if (arr[index].value !== first) {
                 // console.log(first);
                 // console.log("are diff");
-                // areEqual = false;
-            // }
-        // }
-        // return areEqual;
-    // }W
+                areEqual = false;
+            }
+        }
+        return areEqual;
+    }
+
+    useEffect(() => {
+        if (isGameOver) {
+            buttonRef.current.focus();
+            console.log("button focused");
+        }
+    }, [isGameOver]);
 
     function generateArrayOfNumbers(length, max) {
-        console.log("generate");
+        // console.log("generate");
         const arr = new Array(length);
         for (let index = 0; index < arr.length; index++) {
             const number = Math.ceil(Math.random() * max);
@@ -62,7 +73,7 @@ export function Main() {
         return arr;
     }
 
-    console.log(dice);
+    // console.log(dice);
     const buttons = dice.map(obj => {
         return (
             <Die 
@@ -114,15 +125,24 @@ export function Main() {
 
     return (
         <main id="tenzies">
+            <div aria-live="polite" className="sr-only">
+                {isGameOver && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
             <div className="container">
                 {buttons}
             </div>
-            <button className="roll" onClick={() => roll(6)}>
-                {gameWon ? "New Game" : "Roll"}
-            </button>
+            {/* <button className="roll" onClick={() => roll(6)}>
+                {isGameOver ? "New Game" : "Roll"}
+            </button> */}
             {/* <button className="roll" onClick={() => roll(6)}>roll</button> */}
-            {/* <button className="roll" onClick={callback}>{buttonText}</button> */}
             {/* {isGameOver && <button className="roll" onClick={() => startNewGame(10, 6)}>New Game</button>} */}
+            <button 
+                className="roll" 
+                onClick={callback} 
+                ref={buttonRef}
+            >
+                {buttonText}
+            </button>
         </main>
     )
 }
