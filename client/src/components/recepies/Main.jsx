6c5237074e84a00dfe2135df1b2ced1abbe684f3
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-10 15:50:24 +0200

    added language chips elements

--- client/src/components/recepies/Main.jsx
+++ client/src/components/recepies/Main.jsx
@@ -1,4 +1,71 @@
+const languages = [
+    {
+        name: "HTML",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA",
+    },
+    {
+        name: "CSS",
+        backgroundColor: "#328AF1",
+        color: "#F9F4DA",
+    },
+    {
+        name: "JavaScript",
+        backgroundColor: "#F4EB13",
+        color: "#1E1E1E",
+    },
+    {
+        name: "React",
+        backgroundColor: "#2ED3E9",
+        color: "#1E1E1E",
+    },
+    {
+        name: "TypeScript",
+        backgroundColor: "#298EC6",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Node.js",
+        backgroundColor: "#599137",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Python",
+        backgroundColor: "#FFD742",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Ruby",
+        backgroundColor: "#D02B2B",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Assembly",
+        backgroundColor: "#2D519F",
+        color: "#F9F4DA",
+    },
+];
+
 export function Main() {
+    const languageElements = languages.map(lang => {
+        const styles = {
+            color: lang.color, 
+            backgroundColor: lang.backgroundColor,
+        };
+
+        const classes = "chip";
+
+        return (
+            <span
+                key={lang.name} 
+                className={classes} 
+                style={styles}
+            >
+                {lang.name}
+            </span>
+        )
+    });
+
     return (
         <main>
             <header>
@@ -7,7 +74,10 @@ export function Main() {
                     programming world safe from Assembly!</p>
             </header>
 
-            <h2>main</h2>
+            {/* <h2>main</h2> */}
+            <section className="language-chips">
+                {languageElements}
+            </section>
         </main>
     )
 }
