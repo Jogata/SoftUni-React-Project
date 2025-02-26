51009e5faac7e1d6e184382fcb42bc55bb4fa7f7
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-26 17:42:27 +0200

    added navbar component

--- client/src/components/travel/commerce/components/Navbar/Navbar.jsx
+++ client/src/components/travel/commerce/components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
+import "./navbar.css";
+
 export function Navbar() {
     return (
-        <nav>
-            <h1>navbar</h1>
+        <nav className="e-navbar">
+            {/* <h1>navbar</h1> */}
+            <div className="logo">
+                <span>ShopX</span>
+            </div>
+            <div className="links">
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Products</a>
+                    </li>
+                    <li>
+                        <a href="">Contacts</a>
+                    </li>
+                    <li>
+                        <a href="">App</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="nav-icons">
+                <button className="nav-cart">
+                    <i className="ri-shopping-cart-line"></i>
+                    <span className="cart-amount">0</span>
+                </button>
+                <i className="fa fa-user-o"></i>
+            </div>
         </nav>
     )
 }
