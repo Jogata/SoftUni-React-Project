eb41b705127e47365f78a928431de570dbed3ae0
Yonko Nikolov <jonkoslav@abv.bg>
2025-02-27 13:19:29 +0200

    added hero component

--- client/src/components/travel/commerce/components/Hero/Hero.jsx
+++ client/src/components/travel/commerce/components/Hero/Hero.jsx
@@ -1,5 +1,49 @@
+import heroImg from "../../images/hero.png"
+import "./hero.css";
+
 export function Hero() {
     return (
-        <h1>hero</h1>
+        // <h1>hero</h1>
+        <div className="hero">
+            <div className="hero-top">
+                <div className="left">
+                    <h2>Elevate Your Wardrobe</h2>
+                    <h1>Discover Timeless Style and Unmatched Comfort</h1>
+                    <p>Shop the latest trends and classic essentials just for you</p>
+                </div>
+                <div className="right">
+                    <img src={heroImg} alt="Woman model" />
+                </div>
+            </div>
+            <div className="hero-bottom">
+                <div className="hero-content">
+                    <div className="info-icon">
+                        <i className='ri-truck-fill hero-cc-icon'></i>
+                    </div>
+                    <div className="detail">
+                        <h3>Free Shipping</h3>
+                        <p>Free shipping on order</p>
+                    </div>
+                </div>
+                <div className="hero-content">
+                    <div className="info-icon">
+                        <i class="ri-customer-service-2-fill hero-cc-icon"></i>
+                    </div>
+                    <div className="detail">
+                        <h3>24/7 Suuport</h3>
+                        <p>Full support on process</p>
+                    </div>
+                </div>
+                <div className="hero-content">
+                    <div className="info-icon">
+                        <i className='fa fa-credit-card-alt hero-cc-icon'></i>
+                    </div>
+                    <div className="detail">
+                        <h3>Secure Payment</h3>
+                        <p>Ypur payment is secure</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- client/src/components/travel/commerce/pages/Homepage/Home.jsx
+++ client/src/components/travel/commerce/pages/Homepage/Home.jsx
@@ -4,7 +4,6 @@ import { ProductList } from "../../components/ProductList/ProductList";
 export function Home() {
     return (
         <>
-            <h1>home</h1>
             <Hero />
             <ProductList />
         </>
