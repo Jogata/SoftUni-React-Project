fad3c50430700a2c3f2e0dc4b2a8f48a9ecc96de
Yonko Nikolov <jonkoslav@abv.bg>
2025-03-05 19:55:43 +0200

    added footer component

--- client/src/components/travel/commerce/components/Footer/Footer.jsx
+++ client/src/components/travel/commerce/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
+import "./footer.css";
+
 export function Footer() {
     return (
-        <footer>
-            <h1>footer</h1>
-        </footer>
+        <>
+            <footer>
+                <div className="footer-content">
+                    <div className="footer-name">
+                        <h2>ShopX</h2>
+                    </div>
+                    <div className="footer-socials">
+                        <i className="ri-facebook-circle-fill footer-icon"></i>
+                        <i className="ri-instagram-fill footer-icon"></i>
+                        <i className="ri-twitter-line footer-icon"></i>
+                    </div>
+                </div>
+                <div className="copy">
+                    <p>Copyright © ShopX 2024. All rights reserved</p>
+                </div>
+            </footer>
+        </>
     )
 }
