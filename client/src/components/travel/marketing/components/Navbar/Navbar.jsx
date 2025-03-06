72ff9e02435bdd341027e248e0c34fbe1e74a3e5
Yonko Nikolov <jonkoslav@abv.bg>
2025-03-06 14:15:18 +0200

    added navbar component

--- client/src/components/travel/marketing/components/Navbar/Navbar.jsx
+++ client/src/components/travel/marketing/components/Navbar/Navbar.jsx
@@ -1,3 +1,30 @@
+import "./navbar.css";
+
 export function Navbar() {
-    return <h1>navbar comp</h1>
+    return (
+        <nav>
+            <div className="logo">
+                <span>Trevo</span>
+            </div>
+            <div className="links">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="btn">
+                <a href="#">Contact Us</a>
+            </div>
+        </nav>
+    )
 }
