c935336913d61cc7c148bf2d223e238fd4e8df0a
Yonko Nikolov <jonkoslav@abv.bg>
2025-03-22 18:18:36 +0200

    added footer component

--- client/src/components/travel/saas-marketing/components/Footer/Footer.jsx
+++ client/src/components/travel/saas-marketing/components/Footer/Footer.jsx
@@ -2,6 +2,48 @@ import "./footer.css"
 
 export function Footer() {
     return (
-        <h1>footer component</h1>
+        <footer className="footer">
+            <div className="footer-top">
+                <h2>Ready To Get Started?</h2>
+                <p>Join Our Community of Marketers</p>
+                <div className="input">
+                    <input type="email" name="email" placeholder="Enter your email" />
+                    <button>Join Now</button>
+                </div>
+            </div>
+            <hr />
+            <div className="footer-bottom">
+                <div className="footer-left">
+                    <h2>Trevo</h2>
+                    <div className="socials">
+                        <a href="#">
+                            <i className="fa fa-facebook"></i>
+                        </a>
+                        <a href="#">
+                            <i className="fa fa-instagram"></i>
+                        </a>
+                        <a href="#">
+                            <i className="fa fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+                <div className="footer-right">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     )
 }
