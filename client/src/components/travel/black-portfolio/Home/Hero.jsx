3ed58a9972aabbc7b4e912a24865f5f9771eb6ed
Yonko Nikolov <jonkoslav@abv.bg>
2025-03-23 13:33:09 +0200

    added hero component

--- client/src/components/travel/black-portfolio/Home/Hero.jsx
+++ client/src/components/travel/black-portfolio/Home/Hero.jsx
@@ -1,5 +1,29 @@
 import "./hero.css";
+import img from "../images/headshot.jpg";
 
 export function Hero() {
-    return <h1>hero component</h1>
+    const professions = [
+        "Senior Frontend Developer", 
+        "Senior Frontend Engineer"
+    ];
+
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <div className="hero-left">
+                    <h2>I'm Alvin Jonathan</h2>
+                    <h1>{professions[0]}</h1>
+                    <p>
+                        I'm a senior frontend engineer with over 15 
+                        years of successful working experience
+                    </p>
+                </div>
+                <div className="hero-right">
+                    <div className="hero-img">
+                        <img src={img} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
