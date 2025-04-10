717f8f7e8b389e6569364e96fd8d70752d2bdd57
Yonko Nikolov <jonkoslav@abv.bg>
2025-04-10 09:34:44 +0300

    added css for estate details component

--- client/src/components/travel/real-estate-hpg/pages/EstateDetails.jsx
+++ client/src/components/travel/real-estate-hpg/pages/EstateDetails.jsx
@@ -23,21 +23,21 @@ export function EstateDetails() {
     // }
 
     return (
-        <div className="estate-details">
+        <div className="estate-details-page">
             <div className="details-header">
                 <h2 className="name">{estate.name}</h2>
-                <p className="price">{estate.price}</p>
+                <p className="price">$ {estate.price}</p>
             </div>
-            <h3>{estate.country}</h3>
+            <h3 className="country">{estate.country}</h3>
             <hr />
             <div className="estate-features">
                 <p className="feature">
                     <i className="fa fa-bed"></i>
-                    {estate.beds}
+                    {estate.beds} Beds
                 </p>
                 <p className="feature">
                     <i className="fa fa-bath"></i>
-                    {estate.baths}
+                    {estate.baths} Bathrooms
                 </p>
                 <p className="feature">
                     <i className="fa fa-university"></i>
@@ -45,31 +45,31 @@ export function EstateDetails() {
                 </p>
             </div>
             <hr />
-            <img src={estate.image} alt="" />
-            <h3>Property Description</h3>
-            <div className="test">
-                <div className="desc">
+            <img className="estate-image" src={estate.image} alt="" />
+            <div className="info-section">
+                <h3>Property Description</h3>
+                <div className="info-box">
                     <p>PROPERTY TYPE</p>
                     <h4>{estate.property}</h4>
                 </div>
-                <div className="desc">
+                <div className="info-box">
                     <p>STATUS</p>
                     <h4>Available</h4>
                 </div>
-                <div className="desc">
+                <div className="info-box">
                     <p>YEAR BUILT</p>
                     <h4>2020</h4>
                 </div>
-                <div className="desc">
+                <div className="info-box">
                     <p>PROPERTY ID</p>
                     <h4>BVDGYGAYAKAAHE567</h4>
                 </div>
             </div>
             <hr />
-            <p>{estate.description}</p>
-            <p>{estate.description}</p>
-            <p>{estate.description}</p>
-            <p>{estate.description}</p>
+            <p className="description">{estate.description}</p>
+            <p className="description">{estate.description}</p>
+            <p className="description">{estate.description}</p>
+            <p className="description">{estate.description}</p>
 
             <div className="form">
                 <p>Please fill the form for property inspection</p>
@@ -77,12 +77,12 @@ export function EstateDetails() {
                     <h2>Book An Inspection</h2>
                     <div className="input-box">
                         <label htmlFor="name">Full Name *</label>
-                        <input type="text" name="name" id="name" required />
-                        <input type="text" name="surname" id="surname" required />
+                        <input type="text" name="name" id="name" placeholder="First Name" required />
+                        <input type="text" name="surname" id="surname" placeholder="Last Name" required />
                     </div>
                     <div className="input-box">
                         <label htmlFor="email">Email *</label>
-                        <input type="email" name="email" id="email" required />
+                        <input type="email" name="email" id="email" placeholder="example@gmail.com" required />
                     </div>
                     <div className="input-box">
                         <label htmlFor="message">Message</label>
@@ -94,6 +94,7 @@ export function EstateDetails() {
                             placeholder="Hi, I'm interested in this property"
                         ></textarea>
                     </div>
+                    <button>Submit</button>
                 </form>
             </div>
         </div>
