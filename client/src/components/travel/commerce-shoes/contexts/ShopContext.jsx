b37284c9e1e4ccb5c766dc829a6db0dd2704ad80
Yonko Nikolov <jonkoslav@abv.bg>
2025-04-13 12:30:55 +0300

    added data

--- client/src/components/travel/commerce-shoes/contexts/ShopContext.jsx
+++ client/src/components/travel/commerce-shoes/contexts/ShopContext.jsx
@@ -1,46 +1,11 @@
 import { createContext, useState } from "react";
 
-import testimage from '../images/nike-hero.png';
+import { productsData } from "../data";
 
 export const ShopContext = createContext();
 
-const testData = [
-    {
-        id : 1, 
-        image : testimage, 
-        title : 'Men Air Running Shoe',
-        price: 130,
-        category: 'ladies',
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
-    },
-    {
-        id : 2, 
-        image : testimage, 
-        title : 'FitHit Men Non-Slippery Shoe',
-        price: 130,
-        category: 'ladies',
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
-    },
-    {
-        id : 3, 
-        image : testimage, 
-        title : 'Men Air Running Shoe',
-        price: 130,
-        category: 'ladies',
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
-    },
-    {
-        id : 4, 
-        image : testimage, 
-        title : 'FitHit Men Non-Slippery Shoe',
-        price: 130,
-        category: 'ladies',
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
-    }
-]
-
 export function ShopContextProvider({children}) {
-    const [products, setProducts] = useState(testData);
+    const [products, setProducts] = useState(productsData);
 
     return (
         <ShopContext.Provider value={products}>
