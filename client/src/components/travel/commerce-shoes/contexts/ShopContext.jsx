2f5c41ed9c30ac2349d51118aa6dc006c5a04326
Yonko Nikolov <jonkoslav@abv.bg>
2025-04-14 09:31:39 +0300

    added add to cart function

--- client/src/components/travel/commerce-shoes/contexts/ShopContext.jsx
+++ client/src/components/travel/commerce-shoes/contexts/ShopContext.jsx
@@ -6,7 +6,29 @@ export const ShopContext = createContext();
 
 export function ShopContextProvider({children}) {
     const [products, setProducts] = useState(productsData);
+    const [cart, setCart] = useState([]);
 
+    const addToCart = (product, id) => {
+        const newItem = { ...product, amount: 1 };
+        
+        const cartItem = cart.find((item) => {
+            return item.id === id;
+        });
+        
+        if (cartItem) {
+            const newCart = [...cart].map((item) => {
+                if (item.id === id) {
+                    return { ...item, amount: cartItem.amount + 1 };
+                } else {
+                    return item;
+                }
+            });
+            setCart(newCart);
+        } else {
+            setCart([...cart, newItem]);
+        }
+    };
+    
     return (
         <ShopContext.Provider value={products}>
             {children}
