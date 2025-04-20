47322d9de2867fa5a37e52832c070b4f941b9bf0
Yonko Nikolov <jonkoslav@abv.bg>
2025-04-20 11:47:02 +0300

    added css for product details component

--- client/src/components/travel/commerce-shoes/pages/ProductDetails.jsx
+++ client/src/components/travel/commerce-shoes/pages/ProductDetails.jsx
@@ -9,18 +9,18 @@ export function ProductDetails() {
     // console.log(products);
 
     const product = products.find(p => p.id == id);
-    console.log(product);
+    // console.log(product);
 
     return (
-        <div className="products-section">
-            <div className="">
-                <div className="">
+        <div className="product-details-section">
+            {/* <div className=""> */}
+                <div className="product-details-image">
                     <img src={product.image} alt="" className="" />
                 </div>
-                <div className="">
-                    <h2 className="">{product.title}</h2>
-                    <p className="">$ {product.price}</p>
-                    <p className="">{product.description}</p>
+                <div className="product-details">
+                    <h2 className="title">{product.title}</h2>
+                    <p className="price">$ {product.price}</p>
+                    <p className="description">{product.description}</p>
                     <button
                         onClick={() => addToCart(product, id)}
                         className=""
@@ -28,7 +28,7 @@ export function ProductDetails() {
                         ADD TO CART
                     </button>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     )
 }
