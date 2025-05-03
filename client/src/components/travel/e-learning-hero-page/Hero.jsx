a1db91f72bd49c9ae1d3708e9d4ea6aaca2d9fb8
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-03 22:24:15 +0300

    hero section

--- client/src/components/travel/e-learning-hero-page/Hero.jsx
+++ client/src/components/travel/e-learning-hero-page/Hero.jsx
@@ -5,63 +5,87 @@ import student3 from "./images/s4.jpg";
 
 export function Hero() {
     return (
-        <div className="page">
-            <section className="hero-section">
-                <div className="hero-title">
-                    <h1>
-                        <span>Empower</span> Your Future with
-                        Cutting-Edge <span>Skills</span>
-                    </h1>
-                    <p>
-                        Unlock your potential with courses designed to help you thrive in
-                        the digital age. From coding to creative arts, we provide the tools
-                        you need to succeed.
-                    </p>
-                    <div>
-                        <a
-                            href="#" 
-                            className="enroll-btn"
-                        >
-                            Enroll Now
-                        </a>
-                        <a
-                            href="#" 
-                            className="explore-btn"
-                        >
-                            Explore Courses
-                        </a>
-                    </div>
-
-                </div>
-
-                <div className="hero-images">
-                    <div className="images-row">
-                        <img
-                            src={student}
-                            alt="Student"
-                            className="hero-image top-left"
-                        />
-                        <img
-                            src={student1}
-                            alt="Student"
-                            className="hero-image top-right"
-                        />
-                    </div>
-
-                    <div className="images-row">
-                        <img
-                            src={student2}
-                            alt="Student"
-                            className="hero-image bottom-left"
-                        />
-                        <img
-                            src={student3}
-                            alt="Student"
-                            className="hero-image bottom-right"
-                        />
-                    </div>
-                </div>
-            </section>
+        <div className="hero-container">
+            <div className="hero-content">
+                <h1>Turning seconds into forever, 
+                    <span>one frame at a time</span>
+                </h1>
+                <p>
+                    Specialized in capturing amazing pictures 
+                    and moments you would always remember.
+                </p>
+                <button className="hero-btn">Send Us a Message</button>
+            </div>
+            <div className="hero-gallery">
+                <img src={student} alt="" id='s1' />
+                <img src={student1} alt="" id='s2' />
+                <img src={student2} alt="" id='s3' />
+                <img src={student3} alt="" id='s4' />
+                <img src={student1} alt="" id='s5' />
+            </div>
         </div>
-    );
-};
+    )
+}
+
+// export function Hero() {
+//     return (
+//         <div className="page">
+//             <section className="hero-section">
+//                 <div className="hero-title">
+//                     <h1>
+//                         <span>Empower</span> Your Future with
+//                         Cutting-Edge <span>Skills</span>
+//                     </h1>
+//                     <p>
+//                         Unlock your potential with courses designed to help you thrive in
+//                         the digital age. From coding to creative arts, we provide the tools
+//                         you need to succeed.
+//                     </p>
+//                     <div>
+//                         <a
+//                             href="#" 
+//                             className="enroll-btn"
+//                         >
+//                             Enroll Now
+//                         </a>
+//                         <a
+//                             href="#" 
+//                             className="explore-btn"
+//                         >
+//                             Explore Courses
+//                         </a>
+//                     </div>
+
+//                 </div>
+
+//                 <div className="hero-images">
+//                     <div className="images-row">
+//                         <img
+//                             src={student}
+//                             alt="Student"
+//                             className="hero-image top-left"
+//                         />
+//                         <img
+//                             src={student1}
+//                             alt="Student"
+//                             className="hero-image top-right"
+//                         />
+//                     </div>
+
+//                     <div className="images-row">
+//                         <img
+//                             src={student2}
+//                             alt="Student"
+//                             className="hero-image bottom-left"
+//                         />
+//                         <img
+//                             src={student3}
+//                             alt="Student"
+//                             className="hero-image bottom-right"
+//                         />
+//                     </div>
+//                 </div>
+//             </section>
+//         </div>
+//     );
+// };
--- client/src/components/travel/e-learning-hero-page/Navigation.jsx
+++ client/src/components/travel/e-learning-hero-page/Navigation.jsx
@@ -1,58 +1,87 @@
 export function Navigation() {
     return (
-        <div className="page">
-            <nav>
-
-                <div className="logo">
-                    <h2>Edukative</h2>
-                </div>
-
-                <div>
-                    <ul>
-                        <li>
-                            <a 
-                                href="#"
-                                className="nav-link"
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a 
-                                href="#about"
-                                className="nav-link"
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <a 
-                                href="#services"
-                                className="nav-link"
-                            >
-                                Services
-                            </a>
-                        </li>
-                        <li>
-                            <a 
-                                href="#projects"
-                                className="nav-link"
-                            >
-                                Projects
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div>
-                    <a 
-                        href="#contact"
-                        className="nav-link"
-                    >
-                        Contact Us
-                    </a>
-                </div>
-            </nav>
+        <div className="navbar">
+            <div className="nav-logo">
+                <h2>Orange Media</h2>
+            </div>
+            <div className="links">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="btn">
+                <button>Contact Us</button>
+            </div>
         </div>
-    );
-};
+    )
+}
+
+// export function Navigation() {
+//     return (
+//         <div className="page">
+//             <nav>
+
+//                 <div className="logo">
+//                     <h2>Edukative</h2>
+//                 </div>
+
+//                 <div>
+//                     <ul>
+//                         <li>
+//                             <a 
+//                                 href="#"
+//                                 className="nav-link"
+//                             >
+//                                 Home
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a 
+//                                 href="#about"
+//                                 className="nav-link"
+//                             >
+//                                 About
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a 
+//                                 href="#services"
+//                                 className="nav-link"
+//                             >
+//                                 Services
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a 
+//                                 href="#projects"
+//                                 className="nav-link"
+//                             >
+//                                 Projects
+//                             </a>
+//                         </li>
+//                     </ul>
+//                 </div>
+
+//                 <div>
+//                     <a 
+//                         href="#contact"
+//                         className="nav-link"
+//                     >
+//                         Contact Us
+//                     </a>
+//                 </div>
+//             </nav>
+//         </div>
+//     );
+// };
