d0383a869f242c2e05f4275cd56bfb4d3f0d63fd
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-04 15:41:02 +0300

    added navbar component

--- client/src/components/travel/execute-financial-main/components/navbar/Navbar.jsx
+++ client/src/components/travel/execute-financial-main/components/navbar/Navbar.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
+import logo from "../../images/logo.png";
 import './navbar.css';
 
 export function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleClick = () => setIsMenuOpen(!isMenuOpen);
+
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
         <div className='header'>
-            <h1>navbar</h1>
+            <nav className='navbar'>
+                <a href='/' className='logo'>
+                    <img src={logo} alt='logo' />
+                </a>
+                <div className='hamburger' onClick={handleClick}>
+                    <i className="fa fa-bars"></i>
+                </div>
+                <ul className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
+                    <li className='nav-item'>
+                        <a href='/' onClick={closeMenu}>Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#about' onClick={closeMenu}>About</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#testimonials' onClick={closeMenu}>Testimonials</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#demo' onClick={closeMenu}>Demo</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
