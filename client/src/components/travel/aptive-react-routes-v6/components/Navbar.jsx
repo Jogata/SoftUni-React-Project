6b0e5857f03f8027b65a81adc939d3af0b986d2f
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-07 10:34:34 +0300

    added navbar component

--- client/src/components/travel/aptive-react-routes-v6/components/Navbar.jsx
+++ client/src/components/travel/aptive-react-routes-v6/components/Navbar.jsx
@@ -1,5 +1,39 @@
-import './navbar.css';
+import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./navbar.css";
 
 export function Navbar() {
-    return <h1>logo</h1>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    const menuClasses = isMenuOpen ? "nav-menu active" : "nav-menu";
+    const menuButtonClasses = isMenuOpen ? "fa fa-times" : "fa fa-bars";
+
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="logo">
+                    <Link to="/"><img src={logo} alt="" /></Link>
+                </div>
+                <ul className={menuClasses}>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/pricing" className="nav-link">Pricing</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/faq" className="nav-link">FAQ</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/contact" className="nav-link">Contact</Link>
+                    </li>
+                </ul>
+                <button className="hamburger" onClick={toggleMenu}>
+                    <i className={menuButtonClasses}></i>
+                </button>
+            </nav>
+        </header>
+    )
 }
