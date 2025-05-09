342f6e3d91f99e3b5abc32007b73e31efe178a96
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-09 09:03:36 +0300

    added contact component

--- client/src/components/travel/aptive-react-routes-v6/pages/Home.jsx
+++ client/src/components/travel/aptive-react-routes-v6/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Hero } from "../components/Hero";
 import { Pricing } from "../components/Pricing";
 import { Testimonials } from "../components/Testimonials";
 import { Faq } from "../components/Faq";
+import { Contact } from "../components/Contact";
 
 export function Home() {
     return (
@@ -12,6 +13,7 @@ export function Home() {
             <Pricing />
             <Testimonials />
             <Faq />
+            <Contact />
         </div>
     )
 }
