f72593753abbc0d1b3ad08f4a0e5086bc48c5c1b
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-13 09:53:13 +0300

    added dark navigation on scroll functionality

--- client/src/components/travel/galaxy-travel/components/navbar/Navbar.jsx
+++ client/src/components/travel/galaxy-travel/components/navbar/Navbar.jsx
@@ -1,14 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-
 
 export function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
+    const [isDark, setIsDark] = useState(false);
+
+    let navClass = "nav dark-bg";
+    // console.log(window.scrollY);
+
+    if (!isDark) {
+        // console.log(isDark);
+        navClass = "nav";
+    }
+
+    // if (window.scrollY <= 90) {
+    //     console.log(window.scrollY);
+    //     navClass = "nav";
+    // }
+
+    const changeColor = () => {
+        // console.log("color");
+        if (window.scrollY >= 90) {
+            setIsDark(true);
+        } else {
+            setIsDark(false);
+        }
+    }
+
+    useEffect(() => {
+        if (window.scrollY <= 90) {
+            // console.log(window.scrollY);
+            navClass = "nav";
+            setIsDark(false);
+        }
+
+        window.addEventListener("scroll", changeColor);
+    }, [])
+
+        // useEffect(() => {
+        // if (!isDark) {
+        //     console.log(isDark);
+        //     navClass = "nav";
+        // }
+    // }, [isDark])
+
+    // useEffect(() => {
+    //     console.log(window.scrollY);
+    // }, [window.scrollY])
+
     return (
-        <nav className="nav">
+        <nav className={navClass}>
             <Link to="/" className="logo">GLX TRVL</Link>
             <ul className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
                 <li>
@@ -24,9 +68,9 @@ export function Navbar() {
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
-            <div className="hamburger" onClick={toggleMenu}>
+            <button className="hamburger" onClick={toggleMenu}>
                 {isMenuOpen ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
-            </div>
+            </button>
         </nav>
     )
 }
