1f0562c02650e7e74761c4f4003f5e3f338e93ea
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-14 18:04:27 +0300

    added hero component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,7 @@ import Logout from './components/logout/Logout'
 // import { Training } from './components/travel/galaxy-travel/routes/Training';
 // import { Contact } from './components/travel/galaxy-travel/routes/Contact';
 import { Navbar } from './components/travel/defi-react-site/components/navbar/Navbar';
+import { Hero } from './components/travel/defi-react-site/components/hero/Hero';
 import { Footer } from './components/travel/defi-react-site/components/footer/Footer';
 
 function Loader() {
@@ -43,6 +44,7 @@ function App() {
     <>
       <AuthContextProvider>
         <Navbar />
+        <Hero />
         <Footer />
         {/* <Routes>
           <Route path='/' element={<Home />} />
