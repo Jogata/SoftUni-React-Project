dedc42c69d5d7dab6a84b9c0dd4d9c09c6be5ee9
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-15 20:48:09 +0300

    added navbar component

--- client/src/components/travel/real-estate-grid/components/navbar/Navigation.jsx
+++ client/src/components/travel/real-estate-grid/components/navbar/Navigation.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
 import "./navigation.css";
 
 export function Navbar() {
-    return <h1>test</h1>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    const menuClasses = isMenuOpen ? "nav-menu active" : "nav-menu";
+    const menuButtonIconClasses = isMenuOpen ? "fa fa-times" : "fa fa-bars";
+
+    return (
+        <div className="nav">
+            <div className="container">
+                <h1>
+                    <i className="fa fa-home"></i>
+                    <span>Real</span>Estate
+                </h1>
+                <button className="btn">Sign In</button>
+                <ul className={menuClasses}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Search</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <button className="hamburger" onClick={toggleMenu}>
+                    <i className={menuButtonIconClasses}></i>
+                </button>
+            </div>
+        </div>
+    )
 }
