39a9d4f66bd70b9ad330f4de579362358a0f88e9
Yonko Nikolov <jonkoslav@abv.bg>
2025-05-31 12:59:55 +0300

    added navigation component

--- client/src/components/travel/drive-react-css-modules/components/navigation/Navigation.jsx
+++ client/src/components/travel/drive-react-css-modules/components/navigation/Navigation.jsx
@@ -1,9 +1,38 @@
 import styles from './navigation.module.css';
 import logo from '../../images/logo.png';
+import { useState } from 'react';
 
 export function Navigation() {
-  return <h1> 
-    <span><img src={logo} alt="" /></span>
-    navigation
-    </h1>
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const navClass = isNavOpen ? [styles.menu, styles.active].join(' ') : [styles.menu];
+    const menuButtonIconClass = isNavOpen ? "fa fa-times" : "fa fa-bars";
+
+    return (
+        <header className={styles.navbar}>
+            <img src={logo} alt='Logo' />
+            <nav>
+                <ul className={navClass} >
+                    <li>
+                        <a href='/#'>Learn More</a>
+                    </li>
+                    <li>
+                        <a href='/#'>Log in</a>
+                    </li>
+                    <li>
+                        <a href='/#'>Sign up</a>
+                    </li>
+                    <li>
+                        <i className="fa fa-search"></i>
+                    </li>
+                    <li>
+                        <i className="fa fa-user-o"></i>
+                    </li>
+                </ul>
+            </nav>
+            <div onClick={() => setIsNavOpen(!isNavOpen)} className={styles.mobile_btn}>
+                <i className={menuButtonIconClass}></i>
+            </div>
+        </header>
+    );
 }
