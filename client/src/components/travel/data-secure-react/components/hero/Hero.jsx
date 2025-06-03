3f69ddcbc8d3e5c147512d4cd41c9b79b5c613cc
Yonko Nikolov <jonkoslav@abv.bg>
2025-06-03 19:59:12 +0300

    added hero component

---
+++ client/src/components/travel/data-secure-react/components/hero/Hero.jsx
@@ -0,0 +1,16 @@
+import "./hero.css";
+
+export function Hero() {
+    return (
+        <div className="hero">
+            <div className="container">
+                <div className="content">
+                    <h1>Data</h1>
+                    <h1 className="blue">Secured</h1>
+                    <h1>Protection</h1>
+                    <div><button>Learn More</button></div>
+                </div>
+            </div>
+        </div>
+    )
+}
