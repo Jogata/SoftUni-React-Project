37717cc6eeb24e18b8ba305f2c94216e936d2228
Yonko Nikolov <jonkoslav@abv.bg>
2025-06-07 21:32:15 +0300

    added slider to banner component

--- client/src/components/travel/furniture-comforty/components/banner/Banner.jsx
+++ client/src/components/travel/furniture-comforty/components/banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./banner.css";
 
 export function Banner() {
@@ -5,35 +6,70 @@ export function Banner() {
         {
             id: 1,
             title: "Best Furniture collection for your interior",
-            subTitle: "welcome to chairs",
+            subTitle: "welcome to chairs 0",
             image: "/src/components/travel/furniture-comforty/images/banner/banner_image.png"
         },
         {
             id: 2,
             title: "Best Furniture collection for your interior",
-            subTitle: "welcome to chairs",
+            subTitle: "welcome to chairs 1",
             image: "/src/components/travel/furniture-comforty/images/banner/banner_image.png"
         },
         {
             id: 3,
             title: "Best Furniture collection for your interior",
-            subTitle: "welcome to chairs",
+            subTitle: "welcome to chairs 2",
             image: "/src/components/travel/furniture-comforty/images/banner/banner_image.png"
         },
         {
             id: 4,
             title: "Best Furniture collection for your interior",
-            subTitle: "welcome to chairs",
+            subTitle: "welcome to chairs 3",
             image: "/src/components/travel/furniture-comforty/images/banner/banner_image.png"
         },
-    ]
+    ];
+
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const length = products.length;
+    const slideClasses = new Array(products.length).fill("slide");
+    const ids = new Array(products.length).fill(null);
+    ids[0] = "relative";
+    slideClasses[slideIndex] = "slide active";
+    let next = slideIndex + 1;
+    next = next % length;
+    slideClasses[next] = "slide next";
+    let prev = slideIndex - 1;
+    prev = prev + length;
+    prev = prev % length;
+    slideClasses[prev] = "slide prev";
+
+    function autoplay() {
+        let current = slideIndex + 1;
+        current = current % length;
+        setSlideIndex(current);
+    }
+
+    useEffect(() => {
+        const id = setTimeout(() => {
+            autoplay();
+        }, 5000);
+
+        return () => {
+            clearInterval(id);
+        }
+    })
+
+    function changeSlide(index) {
+        setSlideIndex(index);
+    }
 
     return (
-        <div className="banner">
+        <div className="banner max-width">
             <div className="slider">
                 <div className="slider-track">
-                    {products?.map(product => (
-                        <div key={product?.id} className="slide">
+                    {products?.map((product, index) => (
+                        <div key={product?.id} className={slideClasses[index]} id={ids[index]}>
 
                             <div className="slide-text">
                                 <p>{product?.subTitle}</p>
