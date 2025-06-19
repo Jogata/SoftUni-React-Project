1757b6833d9730f33423324460210462c121e4cf
Yonko Nikolov <jonkoslav@abv.bg>
2025-06-19 14:26:06 +0300

    added navigation

--- client/src/components/travel/e-commerce-advanced-filtering/navigation/Navigation.jsx
+++ client/src/components/travel/e-commerce-advanced-filtering/navigation/Navigation.jsx
@@ -1,9 +1,26 @@
 import "./navigation.css";
 
 export function Navigation() {
-  return (
-    <nav>
-        <a href="#">Logo</a>
-    </nav>
-  );
+    return (
+        <nav>
+            <div className="nav-container">
+                <input
+                    className="search-input" 
+                    type="text" 
+                    placeholder="Enter your search shoes."
+                />
+            </div>
+            <div className="profile-container">
+                <a href="#">
+                    <i className="fa fa-heart nav-icons"></i>
+                </a>
+                <a href="">
+                    <i className="fa fa-shopping-cart nav-icons"></i>
+                </a>
+                <a href="">
+                    <i className="fa fa-user-o nav-icons"></i>
+                </a>
+            </div>
+        </nav>
+    );
 };
