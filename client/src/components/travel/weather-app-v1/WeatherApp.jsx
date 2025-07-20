80a0631a9aa88b25f8f200fa36dce97fa4203456
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-20 17:50:53 +0300

    added location info component

--- client/src/components/travel/weather-app-v1/WeatherApp.jsx
+++ client/src/components/travel/weather-app-v1/WeatherApp.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import cloud from "./images/icons/cloud.png";
+import clear from "./images/icons/clear.png";
+import drizzle from "./images/icons/drizzle.png";
+import rain from "./images/icons/rain.png";
+import snow from "./images/icons/snow.png";
 import "./styles.css";
 
 export function WeatherApp() {
@@ -6,9 +11,8 @@ export function WeatherApp() {
     const [location, setLocation] = useState("");
 
     const searchLocation = (e) => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7b17a5afbdd511635fa5708bc0b31451`;
-
-        if (e.key === 'Enter') {
+        if (e.key === "Enter") {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7b17a5afbdd511635fa5708bc0b31451`;
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
@@ -51,6 +55,38 @@ function Location({ data }) {
             </div>
             <div className="description">
                 {data.weather ? <p>{data.weather[0].description}</p> : null}
+            </div>
+        </div>
+    )
+}
+
+function LocationInfo({ data }) {
+    // console.log(data);
+    return (
+        <div className="location-info">
+            <div className="box">
+                {data.sys ? <p>{data.sys.country}</p> : null}
+                <p>Country</p>
+            </div>
+            <div className="box">
+                {data.coord ? <p>{data.coord.lon}</p> : null}
+                <p>Longitude</p>
+            </div>
+            <div className="box">
+                {data.coord ? <p>{data.coord.lat}</p> : null}
+                <p>Latitude</p>
+            </div>
+            <div className="box">
+                {data.main ? <p>{data.main.feels_like.toFixed()}&deg;F</p> : null}
+                <p>Feels</p>
+            </div>
+            <div className="box humidity">
+                {data.main ? <p>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+            </div>
+            <div className="box wind">
+                {data.wind ? <p>{data.wind.speed.toFixed()} mph</p> : null}
+                <p>Wind Speed</p>
             </div>
         </div>
     )
