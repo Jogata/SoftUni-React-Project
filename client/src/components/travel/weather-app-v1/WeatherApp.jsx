4a34a7440b916c4ddfaf0007abc5385b52a7c515
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-20 11:21:15 +0300

    added search field

--- client/src/components/travel/weather-app-v1/WeatherApp.jsx
+++ client/src/components/travel/weather-app-v1/WeatherApp.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import "./styles.css";
 
 export function WeatherApp() {
+    const [data, setData] = useState([]);
+    const [location, setLocation] = useState("");
+
+    const searchLocation = (e) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7b17a5afbdd511635fa5708bc0b31451`;
+
+        if (e.key === 'Enter') {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    setData(data);
+                });
+
+            setLocation("");
+        }
+
+    }
+
     return (
         <div className="weather-app">
-            <h1>test</h1>
+            <div className="search">
+                <input 
+                    type="text" 
+                    value={location} 
+                    onKeyDown={(e) => searchLocation(e)} 
+                    onChange={event => setLocation(event.target.value)} 
+                    // onSubmit={event => console.log(event)} 
+                    placeholder="Enter Location" 
+                />
+            </div>
         </div>
     )
 }
