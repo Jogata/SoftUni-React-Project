3b788263feb3234036eb3f228fc80b52256b76da
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-22 09:44:58 +0300

    added skyrim heavy armor sets page

--- client/src/components/skyrim-armor-sets-page/SkyrimArmorSetsPage.jsx
+++ client/src/components/skyrim-armor-sets-page/SkyrimArmorSetsPage.jsx
@@ -11,19 +11,19 @@ export default function SkyrimArmorSetsPage() {
         <PageNavigation />
 
         <main>
-          <div class="page-content armor-sets details-content">
-            <div class="header">
-              <h1 class="big-fs cursive">
-                <span class="silver decor-1">Armor sets</span>
+          <div className="page-content armor-sets details-content">
+            <div className="header-section">
+              <h1 className="big-fs cursive">
+                <span className="silver decor-1">Armor sets</span>
               </h1>
-              <div class="header-content">
-                <div class="main-description">
+              <div className="header-content">
+                <div className="main-description">
                   <p>An Armor Set is defined as a complete set of armor that includes one matching armor piece for the head, chest, hands and feet.</p>
                   <p>There are several different armor sets available in Skyrim, both in heavy and light variants. Certain armor sets can provide special bonuses if the full set is worn.</p>
                   <p>Most of the sets can be crafted at Forges or improved at a Workbench, by using the Smithing skill. But there is some sets of Armors that belong to a specific faction and can be obtained only from members of that faction or unique sets that can be obtained only during special quests.</p>
                 </div>
-                <div class="image">
-                  <div class="image-mask">
+                <div className="image">
+                  <div className="image-mask">
                     {/* <!-- <img src="./img/00test3/blue-dwarven-full-plate-armor.png" alt=""> --> */}
                     <img src="/skyrim/equipment/armor/sets/hero.png" alt="" />
                     {/* <!-- <img src="./img/00test3/female-warrior-full-body-viewmultiple-angle.jpeg" alt=""> --> */}
@@ -32,10 +32,10 @@ export default function SkyrimArmorSetsPage() {
               </div>
             </div>
             <article>
-              <div class="article-content">
-                <h2 class="cursive">Heavy Armor sets</h2>
+              <div className="article-content">
+                <h2 className="cursive">Heavy Armor sets</h2>
                 <p>There are a total of 19 Heavy armor sets. The armor set contains five pieces: a helmet, boots, gauntlets, a cuirass and greaves, and a shield. Some of the sets can be forged at a blacksmith's forge with a specific materials or bought from merchants, but some sets can only be found as random loot.</p>
-                <ul class="bulled">
+                <ul className="bulled">
                   <li><a>Ancient Nord Armor</a></li>
                   <li><a>Blades Armor</a></li>
                   <li><a>Bonemold Armor</a></li>
@@ -54,17 +54,17 @@ export default function SkyrimArmorSetsPage() {
                   <li><a>Orcish Armor</a></li>
                   <li><a>Stalhrim Armor</a></li>
                   {/* <!-- <li><a>Steel Armor</a></li> --> */}
-                  <li><a class="link" href="./skyrim-steel-plate-armor-set.html">Steel Plate Armor</a></li>
+                  <li><a className="link" href="./skyrim-steel-plate-armor-set.html">Steel Plate Armor</a></li>
                   <li><a>Wolf Armor</a></li>
                 </ul>
-                <a href="./skyrim-heavy-armor-sets.html" class="details-btn">Learn More</a>
+                <Link to="/skyrim-heavy-armor-sets" className="details-btn">Learn More</Link>
               </div>
             </article>
             <article>
-              <div class="article-content">
-                <h2 class="cursive">Light Armor sets</h2>
+              <div className="article-content">
+                <h2 className="cursive">Light Armor sets</h2>
                 <p>There are a total of 19 Light armor sets. The armor set contains five pieces: a helmet, boots, gauntlets, a cuirass and greaves, and a shield. Some of the sets can be forged at a blacksmith's forge with a specific materials or bought from merchants, but some sets can only be found as random loot.</p>
-                <ul class="bulled">
+                <ul className="bulled">
                   <li><a>Chitin Armor</a></li>
                   <li><a>Elven Armor</a></li>
                   <li><a>Forsworn Armor</a></li>
@@ -78,14 +78,14 @@ export default function SkyrimArmorSetsPage() {
                   <li><a>Morag Tong Armor</a></li>
                   <li><a>Penitus Oculatus Armor</a></li>
                   <li><a>Scaled Armor</a></li>
-                  <li><a class="link" href="./skyrim-steel-armor.html">Steel Armor</a></li>
+                  <li><a className="link" href="./skyrim-steel-armor.html">Steel Armor</a></li>
                   <li><a>Shrouded Armor</a></li>
                   <li><a>Skaal Armor</a></li>
                   <li><a>Stalhrim Light Armor</a></li>
                   <li><a>Stormcloak Armor</a></li>
                   <li><a>Stormcloak Officer Armor</a></li>
                 </ul>
-                <a href="./skyrim-light-armor-sets.html" class="details-btn">Learn More</a>
+                <a href="./skyrim-light-armor-sets.html" className="details-btn">Learn More</a>
               </div>
             </article>
           </div>
---
+++ client/src/components/skyrim-heavy-armor-sets/SkyrimHeavyArmorSets.jsx
@@ -0,0 +1,86 @@
+import PageNavigation from "../navigation/PageNavigation";
+import "./skyrim-heavy-armor-sets.css";
+
+import { Link } from 'react-router-dom';
+
+export default function SkyrimHeavyArmorSets() {
+    return (
+        <>
+            <div className="page">
+
+                <PageNavigation />
+
+                <main>
+                    <div className="page-content armor-sets details-content">
+
+                        <div className="header-section">
+                            <h1 className="big-fs cursive">
+                                <span className="silver decor-1">Heavy Armor sets</span>
+                            </h1>
+                            <div className="header-content">
+                                <div className="main-description">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quidem. Repudiandae sequi nulla aliquam, repellat officiis obcaecati velit aut. Earum et, fuga repellendus unde vel harum. Deleniti, soluta. Ea sequi ducimus delectus iusto.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quidem. Repudiandae sequi nulla aliquam, repellat officiis obcaecati velit aut. Earum et, fuga repellendus unde vel harum. Deleniti, soluta. Ea sequi ducimus delectus iusto.</p>
+                                </div>
+                                <div className="image">
+                                    <div className="image-mask mask-1">
+                                        <img src="/skyrim/equipment/armor/blue-dwarven-full-plate-armor.jpeg" alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Steel Plate Armor Set</h2>
+                                <p>The Steel Armor Set can be purchased from Blacksmiths and general goods merchants, found as random loot in chests, worn by mid-level bandit chiefs and hired thugs or forged at a blacksmith's forge with Steel Ingots, Iron Ingots, Leather Strips and requires the Steel Smithing perk. Can be upgraded with a steel ingot at a workbench.</p>
+                                <p>The set has 4 pieces: <a href="./skyrim-steel-plate-armor.html" className="link">Steel Plate Armor</a>, Steel Plate Boots, Steel Plate Helmet, Steel Plate Gauntlets.</p>
+                                <a href="./skyrim-steel-plate-armor-set.html" className="details-btn">Learn More</a>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Dragonplate Armor Set</h2>
+                                <p>Dragonplate Armor is forged from Dragon Bones, Dragon Scales and leather strips. The Dragon Armor perk, which requires a Smithing skill of 100, must be acquired before this armor type can be crafted.</p>
+                                <p>The set has 5 pieces: Dragonplate Armor, Dragonplate Boots, Dragonplate Helmet, Dragonplate Gauntlets, Dragonplate Shield.</p>
+                                <a href="#" className="btn not-active">Learn More</a>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Blades Armor Set</h2>
+                                <a href="#" className="btn not-active">Learn More</a>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Daedric Armor Set</h2>
+                                <a href="#" className="btn not-active">Learn More</a>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Dawnguard Heavy Armor Set</h2>
+                                <a href="#" className="btn not-active">Learn More</a>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Dwarven Armor Set</h2>
+                                <a href="#" className="btn not-active">Learn More</a>
+                            </div>
+                        </article>
+                        <article>
+                            <div className="article-content">
+                                <h2 className="cursive">Ebony Armor Set</h2>
+                                <a href="#" className="btn not-active">Learn More</a>
+                            </div>
+                        </article>
+
+                    </div>
+
+                </main>
+            </div>
+        </>
+    )
+}
