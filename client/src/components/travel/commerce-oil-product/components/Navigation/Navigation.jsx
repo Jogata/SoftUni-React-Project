da7f7ffd413d408dab135128fd465a82c780fe36
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-23 15:39:08 +0300

    added navigation component

--- client/src/components/travel/commerce-oil-product/components/Navigation/Navigation.jsx
+++ client/src/components/travel/commerce-oil-product/components/Navigation/Navigation.jsx
@@ -1,3 +1,28 @@
+import "./navigation.css";
+
 export function Navigation() {
-    return <h1>navigation</h1>
+    return (
+        <nav className="nav" id="top">
+            <div className="navigation">
+                <div className="nav-header">
+                    <a href="#top" className="nav-logo">
+                        Healing <span>Bird</span>
+                    </a>
+                </div>
+                <div className="nav-links">
+                    <ul>
+                        <li><a href="#shop">SHOP</a></li>
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#contact">CONTACT</a></li>
+                        <li><a href="#blog">BLOG</a></li>
+                    </ul>
+                </div>
+                <div className="nav-icons">
+                    <i className="ri-search-line icon"></i>
+                    <i className="fa fa-shopping-cart icon"></i>
+                    <i className="fa fa-user-o icon"></i>
+                </div>
+            </div>
+        </nav>
+    )
 }
