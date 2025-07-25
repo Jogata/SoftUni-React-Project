d273266060d812c74bf55296d1a976d0b28a0eea
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-25 10:31:40 +0300

    added footer component

--- client/src/components/travel/commerce-oil-product/components/Footer/Footer.jsx
+++ client/src/components/travel/commerce-oil-product/components/Footer/Footer.jsx
@@ -1,3 +1,24 @@
+import "./footer.css";
+
 export function Footer() {
-    return <h1>footer</h1>
+    return (
+        <footer className="footer">
+            <div className="footer-body">
+                <div className="right">
+                    <div className="footer-talk">
+                        <h2>Subscribe for more Updates</h2>
+                        <p>
+                            There are updates made on new and existing prosucts.
+                            Subscribe now to be the first to get this exiting updates
+                            about our products.
+                        </p>
+                    </div>
+                </div>
+                <div className="subscribe-form">
+                    <input type="text" placeholder="Email address" />
+                    <button>Subscribe</button>
+                </div>
+            </div>
+        </footer>
+    )
 }
