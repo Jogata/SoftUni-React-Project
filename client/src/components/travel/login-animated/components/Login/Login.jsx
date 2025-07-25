e936062d18d1e5b62bf28bfd5d1b75e8f7087934
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-25 22:39:30 +0300

    set up animated form project

--- client/src/components/travel/login-animated/components/Login/Login.jsx
+++ client/src/components/travel/login-animated/components/Login/Login.jsx
@@ -1,5 +1,69 @@
 // import "./login.css";
 
 export function Login() {
-    return <h1>login</h1>
+    return (
+        <div className="wrapper">
+            <div className="titles">
+                <div className="title login"></div>
+                <div className="title register"></div>
+            </div>
+            <div className="forms">
+                <div className="form-control">
+                    <input type="radio" name="slide" id="login" defaultChecked />
+                    <input type="radio" name="slide" id="register" />
+                    <label htmlFor="login" className="label login">Login</label>
+                    <label htmlFor="register" className="label register">Register</label>
+                    <div className="slider-tab"></div>
+                </div>
+
+                <div className="form-details">
+                    <form className="login">
+                        <pre></pre>
+                        <div className="field">
+                            <input type="text" placeholder="Email address" />
+                        </div>
+                        <div className="field">
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <div className="reset">
+                            <a href="#reset">Forgot password ?</a>
+                        </div>
+                        <div className="submit-btn">
+                            <div className="btn-layer"></div>
+                            <button type="submit">Login</button>
+                        </div>
+                        <div className="redirect">
+                            <span>Don't have an account?</span>
+                            <a href="#register">Register</a>
+                        </div>
+                    </form>
+
+                    <form className="register">
+                        <pre></pre>
+                        <div className="field">
+                            <input type="text" placeholder="Name" />
+                        </div>
+                        <div className="field">
+                            <input type="text" placeholder="Email address" />
+                        </div>
+                        <div className="field">
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <div className="field">
+                            <input type="password" placeholder="Confirm password" />
+                        </div>
+                        <div className="submit-btn">
+                            <div className="btn-layer"></div>
+                            <button type="submit">Register</button>
+                        </div>
+                        <div className="redirect">
+                            <span>Already have an account?</span>
+                            <a href="#register">Login</a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    )
 }
