759dc48ecce1c1bb9affc24e16dfa2695b8af86b
Yonko Nikolov <jonkoslav@abv.bg>
2025-07-29 13:07:18 +0300

    added navigation component

--- client/src/components/travel/crm-website/components/Navigation/Navigation.jsx
+++ client/src/components/travel/crm-website/components/Navigation/Navigation.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+import "./navigation.css";
+
 export function Navigation() {
-    return <h1>navigation</h1>
+    return (
+        <nav className="navbar">
+            <div className="nav-logo">
+                <Link to="/">Cantar</Link>
+            </div>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/pricing">Pricing</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div className="nav-btns">
+                <Link to="/login" className="btn">Login</Link>
+                <Link to="/register" className="btn">Register</Link>
+            </div>
+        </nav>
+    )
 }
