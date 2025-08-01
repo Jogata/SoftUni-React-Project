1c22908885705fdea55d2bdc8e67bc35c74494ba
Yonko Nikolov <jonkoslav@abv.bg>
2025-08-01 12:36:55 +0300

    updated feedbacks component

--- client/src/components/travel/crm-website/components/Feedback/Feedback.jsx
+++ client/src/components/travel/crm-website/components/Feedback/Feedback.jsx
@@ -3,37 +3,73 @@ import { feedbacks } from "../../data";
 import { useState } from "react";
 
 export function Feedback() {
-    const [ current, setCurrent] = useState(0);
+    // const [ current, setCurrent] = useState(0);
+    const [slideIndex, setSlideIndex] = useState(0);
+    console.log("index: ", slideIndex);
+
+    const length = feedbacks.length;
+    const slideClasses = new Array(feedbacks.length).fill("slide");
+    const ids = new Array(feedbacks.length).fill(null);
+    ids[0] = "relative";
+    slideClasses[slideIndex] = "slide active";
+    let next = slideIndex + 1;
+    next = next % length;
+    slideClasses[next] = "slide next";
+    let prev = slideIndex - 1;
+    prev = prev + length;
+    prev = prev % length;
+    slideClasses[prev] = "slide prev";
 
     function nextSlide() {
-        let next = current + 1;
+        let next = slideIndex + 1;
         next = next % feedbacks.length;
-        setCurrent(next);
+        setSlideIndex(next);
     }
 
     function prevSlide() {
-        let prev = current - 1;
+        let prev = slideIndex - 1;
         prev = prev + feedbacks.length;
         prev = prev % feedbacks.length;
-        setCurrent(prev);
+        setSlideIndex(prev);
     }
 
     return (
-        <section className="slider feedbacks">
+        <section className="feedbacks">
             <h2>What Our Clients Are Saying</h2>
-            <div className="feedback">
-                <img src={feedbacks[current].image} alt="" />
-                <h3>{feedbacks[current].name}</h3>
-                <p>{feedbacks[current].comment}</p>
+            <div className="slider">
+                <div className="slider-track">
+                    {feedbacks?.map((feedback, index) => (
+                        <div key={index} className={slideClasses[index]} id={ids[index]}>
+                            <div className="feedback">
+                            <img src={feedback.image} alt="" />
+                            <h3>{feedback.name}</h3>
+                            <p>{feedback.comment}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="control">
+                    <button id="next" onClick={() => nextSlide()}>
+                        next <i className="ri-arrow-left-double-line"></i>
+                    </button>
+                    <button id="prev" onClick={() => prevSlide()}>
+                        prev <i className="ri-arrow-right-double-line"></i>
+                    </button>
+                </div>
             </div>
-            <div className="control">
+            {/* <div className="feedback"> */}
+            {/* <img src={feedbacks[current].image} alt="" /> */}
+            {/* <h3>{feedbacks[current].name}</h3> */}
+            {/* <p>{feedbacks[current].comment}</p> */}
+            {/* </div> */}
+            {/* <div className="control">
                 <button id="next" onClick={() => nextSlide()}>
                     next <i className="ri-arrow-left-double-line"></i>
                 </button>
                 <button id="prev" onClick={() => prevSlide()}>
                     prev <i className="ri-arrow-right-double-line"></i>
                 </button>
-            </div>
+            </div> */}
         </section>
     )
 }
