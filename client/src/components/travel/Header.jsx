a6fc3886116a1d013472d1b46a4d51e68955a0c6
Yonko Nikolov <jonkoslav@abv.bg>
2025-09-13 20:56:57 +0300

    added about component

--- client/src/components/travel/Header.jsx
+++ client/src/components/travel/Header.jsx
@@ -50,16 +50,43 @@ export function Hero() {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     <button>Hit Me Up</button>
                     <div className="icons">
-                        {/* <FaStarOfLife className='one' /> */}
-                        {/* <GiBeveledStar className='two' /> */}
-                        {/* <GiFallingStar className='three' /> */}
-                        {/* <GiFlatStar className='four' /> */}
-                        {/* <MdStar className='five' /> */}
-                        {/* <MdStarHalf className='six' /> */}
+                        <i className='fa fa-star one'></i>
+                        <i className='fa fa-star two'></i>
+                        <i className='fa fa-star three'></i>
+                        <i className='fa fa-star four'></i>
+                        <i className='fa fa-star five'></i>
+                        <i className='fa fa-star six'></i>
                     </div>
                 </div>
                 <div className="hero-img">
                     <img src={man} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export function About() {
+    return (
+        <>
+            <div className="about" id='about'>
+                <div className="about-left">
+                    <img src={man} alt="" />
+                </div>
+                <div className="about-right">
+                    <h2>About Me</h2>
+                    <h3>I'm John Doe, A Fullstack developer</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, odit.
+                        Et corrupti fugiat harum rerum! Sint beatae reprehenderit deleniti expedita
+                        neque suscipit perferendis facilis reiciendis error. Amet aperiam eos ea.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio hic dolorem
+                        esse accusamus atque, impedit, cum quibusdam commodi repellendus dicta earum
+                        veritatis voluptate? Illo incidunt nostrum recusandae rem. Cumque, dolores.
+                    </p>
+                    <button>Hit Me Up</button>
                 </div>
             </div>
         </>
