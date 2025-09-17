617b4df6390878a9b0c91a431aeec50af44707bf
Yonko Nikolov <jonkoslav@abv.bg>
2025-09-17 13:25:51 +0300

    added impact component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ import Logout from './components/logout/Logout'
 // import { Training } from './components/travel/galaxy-travel/routes/Training';
 // import { Contact } from './components/travel/galaxy-travel/routes/Contact';
 import { useEffect, useState } from 'react';
-import { Footer, Hero, Navbar, Projects, Services } from './components/travel/Footer'
+import { Footer, Hero, Impact, Navbar, Projects, Services } from './components/travel/Footer'
 
 function Loader() {
   return (
@@ -85,6 +85,7 @@ function App() {
         <Hero />
         <Services />
         <Projects />
+        <Impact />
         <Footer />
 
           {/* <Routes>
--- client/src/components/travel/Footer.jsx
+++ client/src/components/travel/Footer.jsx
@@ -220,6 +220,55 @@ export function Projects() {
     );
 }
 
+export function Impact() {
+    return (
+        <>
+            <div className="impact">
+                <div className="impact-txt">
+                    <h1>Elevate Your Brand with Orange Media</h1>
+                    <p>
+                        Here's what our company statistics looks like and here
+                        is what our ongoing statistics looks like with our numbers
+                        telling our outstanding performance
+                    </p>
+                </div>
+                <div className="impact-container">
+                    <div className="left">
+                        <img src={pix1} alt="" />
+                    </div>
+                    <div className="right">
+                        <div className="right-num">
+                            <h1>80k</h1>
+                            <p>Satisfied Customers</p>
+                        </div>
+                        <div className="right-num">
+                            <h1>50k</h1>
+                            <p>Product Launch</p>
+                        </div>
+                        <div className="right-num">
+                            <h1>96%</h1>
+                            <p>Trending Campaign</p>
+                        </div>
+                        <div className="right-num">
+                            <h1>10K</h1>
+                            <p>Successful Events</p>
+                        </div>
+                        <div className="right-num">
+                            <h1>96%</h1>
+                            <p>Target Audience</p>
+                        </div>
+                        <div className="right-num">
+                            <h1>10K</h1>
+                            <p>Meetups</p>
+                        </div>
+                    </div>
+                </div>
+                <button>Get Started</button>
+            </div>
+        </>
+    )
+}
+
 export function Footer() {
     return (
         <footer className="footer">
