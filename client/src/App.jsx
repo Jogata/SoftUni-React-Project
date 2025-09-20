3d1545cd1e417d48c5b8109a8a20daeb14a10f17
Yonko Nikolov <jonkoslav@abv.bg>
2025-09-20 21:38:32 +0300

    added hero component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ import Logout from './components/logout/Logout'
 // import { Training } from './components/travel/galaxy-travel/routes/Training';
 // import { Contact } from './components/travel/galaxy-travel/routes/Contact';
 import { useEffect, useState } from 'react';
-import { Navigation } from './components/travel/Footer';
+import { Hero, Navigation } from './components/travel/Footer';
 
 function Loader() {
   return (
@@ -82,6 +82,7 @@ function App() {
         </Routes> */}
 
         <Navigation />
+        <Hero />
 
           {/* <Routes>
             <Route path='/' element={<MainPage />} />
--- client/src/components/travel/Footer.jsx
+++ client/src/components/travel/Footer.jsx
@@ -1,3 +1,5 @@
+import john from "./images/john.jpg";
+
 export function Navigation() {
     return (
         <nav className="navigation">
@@ -10,6 +12,45 @@ export function Navigation() {
         </nav>
     )
 }
+
+export function Hero() {
+    return (
+        <div className="hero" id="home">
+            <div className="hero-content">
+                <div className="hero-left">
+                    <p>Hire Me</p>
+                    <p>Collab</p>
+                </div>
+                <div className="hero-center">
+                    <h1>JOHN</h1>
+                    <h1 className="text-bottom">TRUMBLE</h1>
+                    <img src={john} alt="" className="home-img" />
+                    <p>I'm John Trumble, Frontend Developer at Xendex</p>
+                </div>
+                <div className="hero-right">
+                    <ul className="social-icons">
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-facebook social-icon"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-instagram social-icon"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-linkedin social-icon"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 
 // export function Footer() {
 //     return (
