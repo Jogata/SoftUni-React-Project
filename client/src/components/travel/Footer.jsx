1f53985ffbd55eaac165923034c0e610d75e1c19
Yonko Nikolov <jonkoslav@abv.bg>
2025-09-29 13:48:02 +0300

    updated css

--- client/src/components/travel/Footer.jsx
+++ client/src/components/travel/Footer.jsx
@@ -2,24 +2,24 @@ import derek from "./images/derek.jpg";
 
 export function Navbar() { 
     return ( 
-        <>
+        <nav>
             <div className="navigation">
-                <div className="logo">
+                {/* <div className="logo">
                     <h2>Derek</h2>
-                </div> 
-                <div className="links">
-                    <ul> 
+                </div>  */}
+                {/* <div className="links"> */}
+                    <ul className="links">
                         <li>About</li>
                         <li>Projects</li>
                         <li>Services</li>
                         <li>Contact</li>
-                    </ul> 
-                </div> 
-                <div className="button">
+                    </ul>
+                {/* </div> */}
+                {/* <div className="button">
                     <button>Hire Me</button>
-                </div> 
+                </div>  */}
             </div> 
-        </> 
+        </nav>
     ) 
 }
 
@@ -31,7 +31,10 @@ export function Hero() {
                     <div className="hero-left">
                         <h2>I'm Derek Einstein</h2>
                         <h1>Professional <span>FullStack</span> Developer </h1>
-                        <p>I'm a senior fullstack engineer with over 15 years of industrial working experience.</p>
+                        <p>
+                            I'm a senior fullstack engineer with over 15 years 
+                            of industrial working experience.
+                        </p>
                         <div className="buttons">
                             <button>Hire Me</button>
                             <button>View My Projects</button>
@@ -44,17 +47,19 @@ export function Hero() {
                     </div>
                 </div>
                 <div className="hero-bottom">
-                    <div className="hero-info">
-                        <h1>3+</h1>
-                        <p>Years of Experience</p>
-                    </div>
-                    <div className="hero-info">
-                        <h1>1000+</h1>
-                        <p>Projects Completed</p>
-                    </div>
-                    <div className="hero-info">
-                        <h1>800+</h1>
-                        <p>Satisfied Clients</p>
+                    <div className="inner">
+                        <div className="hero-info">
+                            <h1>3+</h1>
+                            <p>Years of Experience</p>
+                        </div>
+                        <div className="hero-info">
+                            <h1>1000+</h1>
+                            <p>Projects Completed</p>
+                        </div>
+                        <div className="hero-info">
+                            <h1>800+</h1>
+                            <p>Satisfied Clients</p>
+                        </div>
                     </div>
                 </div>
             </div>
