21a433a97e16c480011e82a9939d75d4b1114e47
Yonko Nikolov <jonkoslav@abv.bg>
2025-10-02 13:28:44 +0300

    added coins component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ import Logout from './components/logout/Logout'
 // import { Training } from './components/travel/galaxy-travel/routes/Training';
 // import { Contact } from './components/travel/galaxy-travel/routes/Contact';
 import { useEffect, useState } from 'react';
-import { Navigation } from './components/travel/Header'
+import { CoinTable, Navigation } from './components/travel/Header'
 
 function Loader() {
   return (
@@ -82,6 +82,7 @@ function App() {
         </Routes> */}
 
         <Navigation />
+        <CoinTable />
 
           {/* <Routes>
             <Route path='/' element={<MainPage />} />
--- client/src/components/travel/Header.jsx
+++ client/src/components/travel/Header.jsx
@@ -1,7 +1,7 @@
 export function Navigation() {
   return (
     <>
-        <div className="navbar">
+        <div className="navigation">
             <div className="nav-logo">
                 <h2 className="logo">Crypto Check</h2>
             </div>
@@ -19,6 +19,30 @@ export function Navigation() {
         </div>
     </>
   )
+}
+
+export function CoinTable() {
+    return (
+        <div>
+            <div className="coin-table">
+                <div className="hero-container">
+                    <div className="hero">
+                        <h1>Your Gateway to the Latest Crypto Price Trends</h1>
+                        <p>
+                            Get real-time insights on Bitcoin, Ethereum and
+                            altcoins. Analyze trends, make informed decisions
+                            and take control of your investments with ease.
+                        </p>
+                        <form>
+                            <input type="search" name="search" id="search" placeholder="Search crypto" required />
+                            <button type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="crypto-table"></div>
+            </div>
+        </div>
+    )
 }
 
 // export function Header() {
