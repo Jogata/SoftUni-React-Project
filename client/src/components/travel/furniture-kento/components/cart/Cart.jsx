bde84969f2825c6195b737f9617c3ea66aba83e1
Yonko Nikolov <jonkoslav@abv.bg>
2025-10-14 22:54:25 +0300

    updated cart component

--- client/src/components/travel/furniture-kento/components/cart/Cart.jsx
+++ client/src/components/travel/furniture-kento/components/cart/Cart.jsx
@@ -6,14 +6,16 @@ export function Cart() {
     const cart = productsData;
 
     return (
-        <div>
+        <>
             <Navigation />
             <div className="cart-container">
-                <div className="cart-left">
+                <div className="cart-table">
                     <div className="cart-header">
                         <h1>Shopping Cart</h1>
                         <h1>Items: ({cart.length})</h1>
-                        <i className="ri-delete-bin-line delete-cart"></i>
+                        <button className="delete-btn">
+                        <i className="ri-delete-bin-line"></i>
+                        </button>
                     </div>
                     <div className="cart-header">
                         <span>Product Description</span>
@@ -23,7 +25,7 @@ export function Cart() {
                     </div>
                     <div className="cart-details">
                         {cart.length > 0 ? (
-                            cart.map((item) => (
+                            cart.map(item => (
                                 <h1>{item.name}</h1>
                             ))
                         ) : (
@@ -31,7 +33,32 @@ export function Cart() {
                         )}
                     </div>
                 </div>
+                <div className="cart-right">
+                    <h2>Cart Summary</h2>
+                    <div className="summary-item">
+                        <span>Items : </span>
+                        <span>{cart.length}</span>
+                    </div>
+                    <div className="summary-item">
+                        <span>Subtotal</span>
+                        <span> $ {cart.length}</span>
+                    </div>
+                    <div className="summary-item">
+                        <span>Shipping</span>
+                        <span>Free</span>
+                    </div>
+                    <div className="summary-item">
+                        <span>Promo Code</span>
+                        <input type="text" placeholder="Enter your code" />
+                        <button className="apply-btn">Apply</button>
+                    </div>
+                    <div className="summary-item total-cost">
+                        <span>Total Cost</span>
+                        <span>$ {cart.length} </span>
+                    </div>
+                    <button className="checkout">CHECKOUT</button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
