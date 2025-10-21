d4901de67cc5322aadbde19cfa0f45998d8d25d8
Yonko Nikolov <jonkoslav@abv.bg>
2025-10-21 11:49:23 +0300

    removed total and amount states from context

--- client/src/components/travel/furniture-kento/context/ShopContext.jsx
+++ client/src/components/travel/furniture-kento/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { productsData } from "../data";
 
 const data = productsData.map(p => {
@@ -14,31 +14,46 @@ export function ShopContextProvider({ children }) {
 
     const [ cart, setCart ] = useState(data);
 
-    const [ itemsAmount, setItemsAmount ] = useState(0);
+    // const [ itemsAmount, setItemsAmount ] = useState(0);
+    const itemsAmount = cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.amount;
+    }, 0);
+    console.log('Amount:', itemsAmount);
+    // setItemsAmount(amount);
 
-    const [ total, setTotal ] = useState(0);
+    // const [ total, setTotal ] = useState(0);
 
-    useEffect(() => {
-        if (cart) {
-            const amount = cart.reduce((accumulator, currentItem) => {
-                return accumulator + currentItem.amount;
-            }, 0);
-            console.log('Amount:', amount);
-            setItemsAmount(amount);
+    const total = cart.reduce((accumulator, currentItem) => {
+        const priceAsNumber = parseFloat(currentItem.price);
+        if (isNaN(priceAsNumber)) {
+            return accumulator;
         }
-    }, [cart]);
+        return accumulator + priceAsNumber * currentItem.amount;
+    }, 0);
+    console.log('Total:', total);
+    // setTotal(total);
 
-    useEffect(() => {
-        const total = cart.reduce((accumulator, currentItem) => {
-            const priceAsNumber = parseFloat(currentItem.price);
-            if (isNaN(priceAsNumber)) {
-                return accumulator;
-            }
-            return accumulator + priceAsNumber * currentItem.amount;
-        }, 0);
-        console.log('Total:', total);
-        setTotal(total);
-    }, [cart]);
+    // useEffect(() => {
+    //     if (cart) {
+    //         const amount = cart.reduce((accumulator, currentItem) => {
+    //             return accumulator + currentItem.amount;
+    //         }, 0);
+    //         console.log('Amount:', amount);
+    //         setItemsAmount(amount);
+    //     }
+    // }, [cart]);
+
+    // useEffect(() => {
+    //     const total = cart.reduce((accumulator, currentItem) => {
+    //         const priceAsNumber = parseFloat(currentItem.price);
+    //         if (isNaN(priceAsNumber)) {
+    //             return accumulator;
+    //         }
+    //         return accumulator + priceAsNumber * currentItem.amount;
+    //     }, 0);
+    //     console.log('Total:', total);
+    //     setTotal(total);
+    // }, [cart]);
 
     function removeItemFromCart(id) {
         const newCart = cart.filter((item) => {
@@ -48,86 +63,33 @@ export function ShopContextProvider({ children }) {
     }
 
     function addToCart(product) {
-        // const newItem = { ...product, amount: 1 };
-        // console.log(newItem)
-        // console.log(`item ${product.title} added to cart`)
-
-        // const cartItem = cart.find((item) => {
-        //     return item.id === product.id;
-        // });
-        // console.log(cartItem);
         const index = cart.findIndex(item => {
             return item.id === product.id;
         });
         // console.log(index);
 
-        // if (cartItem) {
         if (index >= 0) {
-            // const product = products[index];
-            // const newCart = [...cart];
-            // const newItem = {...newCart[index]};
-            // newItem.amount = newItem.amount + 1;
-            // newCart[index] = newItem;
-            // const newCart = [...cart].map(item => {
-            //     if (item.id === product.id) {
-            //         return { ...item, amount: cartItem.amount + 1 };
-            //     } else {
-            //         return item;
-            //     }
-            // });
-            // setCart(newCart);
             increaseAmount(product.id);
         } else {
-            const product = products[index];
+            // const product = products[index];
             const newItem = {...product, amount: 1};
             setCart([...cart, newItem])
         }
 
     }
-    // console.log(cart);
 
     function increaseAmount(id) {
         const index = cart.findIndex(item => {
             return item.id === id;
         });
-        console.log(index);
-        // const cartItem = cart.find((item) => item.id === id);
-        // addToCart(cartItem);
-        // const product = products[index];
+        // console.log(index);
+
         const newCart = [...cart];
         const newItem = {...newCart[index]};
         newItem.amount = newItem.amount + 1;
         newCart[index] = newItem;
-        // const newCart = [...cart].map(item => {
-        //     if (item.id === product.id) {
-        //         return { ...item, amount: cartItem.amount + 1 };
-        //     } else {
-        //         return item;
-        //     }
-        // });
         setCart(newCart);
     }
-
-    // function decreaseAmount(id) {
-    //     const cartItem = cart.find((item) => {
-    //         return item.id === id;
-    //     });
-
-    //     if (cartItem) {
-    //         const newCart = cart.map((item) => {
-    //             if (item.id === id) {
-    //                 return { ...item, amount: cartItem.amount - 1 };
-    //             } else {
-    //                 return item;
-    //             }
-    //         });
-    //         setCart(newCart);
-    //     } else {
-    //         if (cartItem.amount < 2) {
-    //             removeItemFromCart(id);
-    //         }
-    //     }
-    // }
 
     function decreaseAmount(id) {
         const index = cart.findIndex(item => {
@@ -150,8 +112,20 @@ export function ShopContextProvider({ children }) {
         setCart([]);
     }
 
+    const context = {
+        products, 
+        cart, 
+        itemsAmount, 
+        total, 
+        addToCart, 
+        increaseAmount, 
+        decreaseAmount, 
+        removeItemFromCart,
+        clearCart
+    }
+
     return (
-        <ShopContext.Provider value={{ products, cart, clearCart, itemsAmount, total, addToCart, increaseAmount, decreaseAmount, removeItemFromCart }}>
+        <ShopContext.Provider value={context}>
             {children}
         </ShopContext.Provider>
   )
