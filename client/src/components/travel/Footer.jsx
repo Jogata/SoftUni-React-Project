0f1a21b3f8fdb68715521d41aa72b7fd22697d2c
Yonko Nikolov <jonkoslav@abv.bg>
2025-11-08 17:11:19 +0200

    added navigation component

--- client/src/components/travel/Footer.jsx
+++ client/src/components/travel/Footer.jsx
@@ -1,19 +1,51 @@
-export function Footer() {
-    return (
-        <div className="footer">
-            <div className="footer-content">
-                <div className="footer-name">
-                    <h2>Nika Shoes</h2>
+import { useState } from "react";
+
+export function Navigation() {
+    const [Mobile, setMobile] = useState(false);
+
+    const iconClasses = Mobile ? "fa fa-times close" : "fa fa-bars open";
+    const navClasses = Mobile ? "nav-links" : "links";
+
+    return ( 
+        <div>
+            <div className="navigation">
+                <div className="nav-logo">
+                    <h2>CREVA</h2>
+                </div> 
+                <div className={navClasses} onClick={() => setMobile(false)}>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                    </ul>
+                </div> 
+                <div className="button">
+                    <button>Booking</button>
                 </div>
-                <div className="footer-socials">
-                    <i className="fa fa-facebook footer-icon"></i>
-                    <i className="fa fa-instagram footer-icon"></i>
-                    <i className="fa fa-twitter footer-icon"></i>
-                </div>
-            </div>
-            <div className="copy">
-                <p>Copyright © Nika Shoes 2024. All rights reserved</p>
-            </div>
-        </div>
-    )
-}
+                <button className="toggle" onClick={() => setMobile(!Mobile)}>
+                    <i className={iconClasses}></i>
+                </button>
+            </div> 
+        </div> 
+    ) 
+} 
+
+// export function Footer() {
+//     return (
+//         <div className="footer">
+//             <div className="footer-content">
+//                 <div className="footer-name">
+//                     <h2>Nika Shoes</h2>
+//                 </div>
+//                 <div className="footer-socials">
+//                     <i className="fa fa-facebook footer-icon"></i>
+//                     <i className="fa fa-instagram footer-icon"></i>
+//                     <i className="fa fa-twitter footer-icon"></i>
+//                 </div>
+//             </div>
+//             <div className="copy">
+//                 <p>Copyright © Nika Shoes 2024. All rights reserved</p>
+//             </div>
+//         </div>
+//     )
+// }
