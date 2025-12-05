49a9f54c97c7ba641d2ee3c1bafa24bf86b39ad9
Yonko Nikolov <jonkoslav@abv.bg>
2025-12-05 21:53:15 +0200

    practice

--- client/src/components/travel/Footer.jsx
+++ client/src/components/travel/Footer.jsx
@@ -1343,6 +1343,86 @@ export function ShoppingCart() {
     );
 }
 
+// ======================================= Challenge 2: Remove an item from the shopping cart  =================================================
+
+const initialProducts2 = [{
+    id: 0,
+    name: "Baklava",
+    count: 1,
+}, {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+}, {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+}];
+
+export function ShoppingCartRemove() {
+    const [
+        products,
+        setProducts
+    ] = useState(initialProducts2)
+
+    function handleIncreaseClick(productId) {
+        setProducts(products.map(product => {
+            if (product.id === productId) {
+                return {
+                    ...product,
+                    count: product.count + 1
+                };
+            } else {
+                return product;
+            }
+        }))
+    }
+
+    function handleDecreaseClick(productId) {
+        let nextProducts = products.map(product => {
+            if (product.id === productId) {
+                return {
+                    ...product,
+                    count: product.count - 1
+                };
+            } else {
+                return product;
+            }
+        });
+        
+        nextProducts = nextProducts.filter(p =>
+            p.count > 0
+        );
+
+        setProducts(nextProducts);
+    }
+
+    return (
+        <ul>
+            {products.map(product => (
+                <li key={product.id}>
+                    {product.name}
+                    {" "}
+                    (<b>{product.count}</b>)
+                    <button onClick={() => {
+                        handleIncreaseClick(product.id);
+                    }}>
+                        +
+                    </button>
+                    <button onClick={() => {
+                        handleDecreaseClick(product.id);
+                    }}>
+                        -
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+// ========================================================================================
+// ========================================================================================
+
 // export function Footer() {
 //     return (
 //         <div className="footer">
