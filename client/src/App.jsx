fbe4b37ac58b15db1d676e7a27c12c44b68a8d35
Yonko Nikolov <jonkoslav@abv.bg>
2025-12-07 23:28:28 +0200

    practice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,7 +38,7 @@ import Logout from './components/logout/Logout'
 // import { Training } from './components/travel/galaxy-travel/routes/Training';
 // import { Contact } from './components/travel/galaxy-travel/routes/Contact';
 import { useEffect, useState } from 'react';
-import { FeedbackForm, FormWithRedundantState, Picture, Picture2, Storybooks, TestForm, TravelPlan } from './components/travel/Header';
+import { FeedbackForm, FlatTravelPlan, FormWithRedundantState, Picture, Picture2, Storybooks, TestForm, TravelPlan } from './components/travel/Header';
 
 function Loader() {
   return (
@@ -102,7 +102,9 @@ function App() {
         <FormWithRedundantState />
         </div> */}
         {/* <hr /> */}
-        <TravelPlan />
+        {/* <TravelPlan /> */}
+        {/* <hr /> */}
+        <FlatTravelPlan />
         <hr />
 
           <Routes>
