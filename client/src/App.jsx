f8e9f3eb1658680a365b160077ab4e08aa221930
Yonko Nikolov <jonkoslav@abv.bg>
2025-12-18 19:34:12 +0200

    practice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,7 +38,7 @@ import Logout from './components/logout/Logout'
 // import { Training } from './components/travel/galaxy-travel/routes/Training';
 // import { Contact } from './components/travel/galaxy-travel/routes/Contact';
 import { useEffect, useState } from 'react';
-import { Accordion, Accordion2, Counters, FancyCounter, FancyCounter2, FeedbackForm, FilterableList, GreetForm, Pagination, RaceCondition, ResetingCounter, SyncedInputs, TestCounterRenderProp, TestMouseTrackerRenderProp, TestUsersRenderProps, WithoutTanstackQuery } from './components/travel/Header';
+import { Accordion, Accordion2, Counters, FancyCounter, FancyCounter2, FancyCounter3, FeedbackForm, FilterableList, GreetForm, Pagination, RaceCondition, ResetingCounter, SyncedInputs, TestCounterRenderProp, TestMouseTrackerRenderProp, TestUsersRenderProps, WithoutTanstackQuery } from './components/travel/Header';
 
 function Loader() {
   return (
@@ -118,7 +118,9 @@ function App() {
         {/* <hr /> */}
         {/* <FancyCounter2 /> */}
         {/* <hr /> */}
-        <ResetingCounter />
+        {/* <ResetingCounter /> */}
+        {/* <hr /> */}
+        <FancyCounter3 />
         <hr />
 
           <Routes>
--- client/src/components/travel/Header.jsx
+++ client/src/components/travel/Header.jsx
@@ -758,6 +758,57 @@ function Counter3({ isFancy }) {
 
 // ============================================================================
 
+export function FancyCounter3() {
+    const [isFancy, setIsFancy] = useState(false);
+
+    return (
+        <div className="test-section">
+            {isFancy && <Counter5 isFancy={false} />}
+            <Counter5 isFancy={false} />
+            <label>
+                <input
+                    type="checkbox"
+                    checked={isFancy}
+                    onChange={e => {
+                        setIsFancy(e.target.checked)
+                    }}
+                />
+                Use fancy styling
+            </label>
+        </div>
+    );
+}
+
+function Counter5({ isFancy }) {
+    const [score, setScore] = useState(0);
+    const [hover, setHover] = useState(false);
+
+    let className = "counter";
+
+    if (hover) {
+        className += " hover";
+    }
+    
+    if (isFancy) {
+        className += " fancy";
+    }
+
+    return (
+        <div
+            className={className}
+            onPointerEnter={() => setHover(true)}
+            onPointerLeave={() => setHover(false)}
+        >
+            <h1>{score}</h1>
+            <button onClick={() => setScore(score + 1)}>
+                Add one
+            </button>
+        </div>
+    );
+}
+
+// ============================================================================
+
 export function ResetingCounter() {
     const [isPaused, setIsPaused] = useState(false);
 
