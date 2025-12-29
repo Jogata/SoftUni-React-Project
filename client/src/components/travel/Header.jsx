e897ce69a8e883a01247bdc9b497f911e1bac568
Yonko Nikolov <jonkoslav@abv.bg>
2025-12-29 19:39:52 +0200

    added footer component

--- client/src/components/travel/Header.jsx
+++ client/src/components/travel/Header.jsx
@@ -15,13 +15,54 @@ export function Navbar() {
                 </div>
                 <div className="prudento-nav-btn">
                     <a href="#contact">
-                        <button>Contact Us</button>
+                        Contact Us
                     </a>
                 </div>
             </div>
         </div>
     )
 }
+
+export function Footer() {
+    return (
+        <footer className="footer-container">
+            <div className="footer-top">
+                <h3 className="footer-title">Prudento</h3>
+                <p className="footer-address">
+                    123, Steet Name, Close Axis, State, Country
+                </p>
+                <div className="social-icons">
+                    <a href="#">
+                        <i className="fa fa-facebook-f"></i>
+                    </a>
+                    <a href="#">
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                    <a href="#">
+                        <i className="fa fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                        <i className="fa fa-google-plus"></i>
+                    </a>
+                    <a href="#">
+                        <i className="fa fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <ul className="footer-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Testimonial</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <p className="copyright">
+                    Copyright © Prudento 2025. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+};
 
 // export function Header() {
 //     return (
