85dabb73f864c1dfe86bd12153583ac575aaab77
Yonko Nikolov <jonkoslav@abv.bg>
2026-01-01 12:44:27 +0200

    added navbar component

--- client/src/components/travel/Footer.jsx
+++ client/src/components/travel/Footer.jsx
@@ -1,3 +1,74 @@
+import { useState } from "react";
+
+export function Navbar() {
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
+    return (
+        <nav>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <img src="/logo.png" alt="CodeFlow" />
+                        </div>
+                        <span>
+                            <span>Code</span>
+                            <span>Flow</span>
+                        </span>
+                    </div>
+
+                    <div>
+                        <a href="#features">
+                            Features
+                        </a>
+                        <a href="#pricing">
+                            Pricing
+                        </a>
+                        <a href="#testimonials">
+                            Testimonials
+                        </a>
+                    </div>
+
+                    <button
+                        onClick={() => setMobileMenuIsOpen(prev => !prev)}
+                    >
+                        {mobileMenuIsOpen ? (
+                            <i className="ri-close-line"></i>
+                        ) : (
+                            <i className="ri-menu-line"></i>
+                        )}
+                    </button>
+                </div>
+            </div>
+
+            {mobileMenuIsOpen && (
+                <div>
+                    <div>
+                        <a
+                            href="#features"
+                            onClick={() => setMobileMenuIsOpen(false)}
+                        >
+                            Features
+                        </a>
+                        <a
+                            href="#pricing"
+                            onClick={() => setMobileMenuIsOpen(false)}
+                        >
+                            Pricing
+                        </a>
+                        <a
+                            href="#testimonials"
+                            onClick={() => setMobileMenuIsOpen(false)}
+                        >
+                            Testimonials
+                        </a>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+}
+
 export function Footer() {
     return (
         <div className="footer">
