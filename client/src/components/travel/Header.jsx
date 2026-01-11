f6f35e3fb0dc7f2a012092fff76de4bd51164a16
Yonko Nikolov <jonkoslav@abv.bg>
2026-01-11 15:25:49 +0200

    added hero section component

--- client/src/components/travel/Header.jsx
+++ client/src/components/travel/Header.jsx
@@ -2,7 +2,10 @@ import { useEffect, useRef, useState } from "react";
 
 export function Home() {
     return (
-        <AnimatedBackground />
+        <>
+            <AnimatedBackground />
+            <HeroSection />
+        </>
     )
 }
 
@@ -14,11 +17,13 @@ export function NotFound() {
 
 function AnimatedBackground() {
     const [stars, setStars] = useState([]);
-    const [meteors, setMeteors] = useState([]);
+    // const [meteors, setMeteors] = useState([]);
+    // const meteors = new Array(4).fill(<Meteor />);
+    // console.log(meteors.length);
 
     useEffect(() => {
         generateStars();
-        generateMeteors();
+        // generateMeteors();
 
         const handleResize = () => {
             console.log("resize");
@@ -52,27 +57,27 @@ function AnimatedBackground() {
         setStars(newStars);
     };
 
-    const generateMeteors = () => {
-        const numberOfMeteors = 4;
-        const newMeteors = new Array(numberOfMeteors);
+    // const generateMeteors = () => {
+    //     const numberOfMeteors = 4;
+    //     const newMeteors = new Array(numberOfMeteors);
 
-        for (let i = 0; i < numberOfMeteors; i++) {
-            newMeteors[i] = ({
-                id: i,
-                size: Math.random() * 2 + 1,
-                x: Math.random() * 100,
-                y: Math.random() * 20,
-                delay: Math.random() * 15,
-                animationDuration: Math.random() * 3 + 3,
-            });
-        }
+    //     for (let i = 0; i < numberOfMeteors; i++) {
+    //         newMeteors[i] = ({
+    //             id: i,
+    //             size: Math.random() * 2 + 1,
+    //             x: Math.random() * 100,
+    //             y: Math.random() * 20,
+    //             delay: Math.random() * 15,
+    //             animationDuration: Math.random() * 3 + 3,
+    //         });
+    //     }
 
-        setMeteors(newMeteors);
-    };
+    //     setMeteors(newMeteors);
+    // };
 
     return (
         <div className="animated-bg">
-            {/* {stars.map((star) => (
+            {stars.map((star) => (
                 <div
                     key={star.id}
                     className="star"
@@ -85,9 +90,15 @@ function AnimatedBackground() {
                         animationDuration: star.animationDuration + "s",
                     }}
                 />
-            ))} */}
+            ))}
 
             <Meteor />
+            <Meteor />
+            <Meteor />
+            <Meteor />
+
+            {/* {meteors.map((meteor, index) => <Meteor key={index} />)} */}
+            {/* {meteors.map((meteor, index) => meteor)} */}
 
             {/* {meteors.map((meteor) => (
                 <div
@@ -109,28 +120,26 @@ function AnimatedBackground() {
 
 function Meteor() {
     const [meteor, setMeteor] = useState({
-        // id: 0,
         size: Math.random() * 2 + 1,
         x: Math.random() * 100,
         y: Math.random() * 20,
-        delay: Math.random() * 15,
-        animationDuration: Math.random() * 3 + 3,
+        animationDuration: 10,
     });
 
-    // const id = useRef(0);
-    console.log(meteor);
+    const id = useRef(0);
+    // console.log(meteor);
 
     useEffect(() => {
         // let id = 0;
-        const duration = Math.random() * 3 + 3;
+        const duration = Math.random() * 3 + 10;
 
-        const id = setTimeout(() => {
+        const timeoutID = setTimeout(() => {
             // id.current++;
             // const newId = id.current;
             const newMeteor = {
                 // id: i,
                 size: Math.random() * 2 + 1,
-                x: Math.random() * 100,
+                x: Math.random() * 70,
                 y: Math.random() * 20,
                 // delay: Math.random() * 15,
                 animationDuration: duration,
@@ -140,25 +149,35 @@ function Meteor() {
             console.log(newMeteor);
             // if (newId == id.current) {
             setMeteor(newMeteor);
-            // id.current++;
+            id.current++;
             // } else {
                 // console.log("no match");
             // }
-        }, duration * 1000);
+        }, duration * 1000 + 3000);
 
-        return () => clearInterval(id);
-    }, [meteor])
+        return () => clearInterval(timeoutID);
+    }, [id.current])
+
+    return (
+        <MeteorElement meteor={meteor} key={id.current} />
+    )
+}
+
+function MeteorElement({ meteor }) {
+    useEffect(() => {
+        return () => {
+            console.log("clear");
+        }
+    })
 
     return (
         <div
-            // key={meteor.id}
             className="meteor"
             style={{
                 width: meteor.size * 50 + "px",
                 height: meteor.size * 2 + "px",
                 left: meteor.x + "%",
                 top: meteor.y + "%",
-                // animationDelay: meteor.delay,
                 animationDuration: meteor.animationDuration + "s",
             }}
         />
@@ -245,6 +264,49 @@ export function Navigation() {
         </nav>
     );
 };
+
+
+export function HeroSection() {
+    return (
+        <section
+            id="hero"
+            className="hero-section"
+        >
+            <div className="container">
+                {/* <div> */}
+                    <h1>
+                        <span className="fade-in">Hi, I'm</span>
+                        <span className="fade-in primary">
+                            {" "}
+                            Software
+                        </span>
+                        <span className="fade-in">
+                            {" "}
+                            Developer
+                        </span>
+                    </h1>
+
+                    <p className="fade-in">
+                        I create stellar web experiences with modern technologies.
+                        Specializing in front-end development, I build interfaces 
+                        that are both beautiful and functional.
+                    </p>
+
+                    <div className="button">
+                        <a href="#projects" className="cosmic-button">
+                            View My Work
+                        </a>
+                    </div>
+                {/* </div> */}
+            </div>
+
+            <div className="scroll">
+                <span className=""> Scroll </span>
+                <i className="fa fa-angle-double-down"></i>
+            </div>
+        </section>
+    );
+}
 
 // export function Header() {
 //     return (
